9cfa77b82bee2de558fa5f2766fa483ee5fb4b9e
David Annebicque <david.annebicque@gmail.com>
2018-05-09 15:03:13 +0200

    Installation

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -10,7 +10,7 @@ apt-get update -yqq
 #apt-get install git -yqq
 #apt-get install git libicu-dev libmcrypt-dev libcurl4-openssl-dev -yqq
 
-apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev phpunit -yqq
+apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
 
 
 # Install phpunit, the tool that we will use for testing
