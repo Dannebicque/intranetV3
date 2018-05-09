b51c5ffa12a503b928c1961aae2f74fa73d9561c
David Annebicque <david.annebicque@gmail.com>
2018-05-09 15:00:47 +0200

    Installation

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,7 +8,10 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 #apt-get install git -yqq
-apt-get install git libicu-dev libmcrypt-dev libcurl4-openssl-dev -yqq
+#apt-get install git libicu-dev libmcrypt-dev libcurl4-openssl-dev -yqq
+
+apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev phpunit -yqq
+
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -16,4 +19,4 @@ chmod +x /usr/local/bin/phpunit
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install mbstring  pdo_mysql curl json intl gd xml zip bz2 opcache
+docker-php-ext-install pdo_mysql intl gd xml zip bz2 opcache
