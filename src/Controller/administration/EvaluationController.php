57295cb59ad70069956febcefd9cdf9ca3e624c8
David Annebicque <david.annebicque@gmail.com>
2018-07-21 17:29:45 +0200

    Visibilité et édition des évaluations en mode administration

--- src/Controller/administration/EvaluationController.php
+++ src/Controller/administration/EvaluationController.php
@@ -3,11 +3,10 @@
 namespace App\Controller\administration;
 
 use App\Controller\BaseController;
+use App\Entity\Constantes;
 use App\Entity\Evaluation;
 use App\Entity\Semestre;
-use App\MesClasses\MyEvaluations;
-use App\MesClasses\MyExport;
-use App\Repository\NoteRepository;
+use App\Repository\EvaluationRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,6 +20,58 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class EvaluationController extends BaseController
 {
+
+    /**
+     * @param          $etat
+     * @param Semestre $semestre
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/visibilite/semestre/{semestre}/{etat}", name="administration_evaluation_visibilite_all",
+     *                                                  methods={"GET"})
+     */
+    public function visibiliteAll(EvaluationRepository $evaluationRepository, $etat, Semestre $semestre)
+    {
+        $evals = $evaluationRepository->findBySemestre($semestre);
+
+
+        /** @var Evaluation $eval */
+        foreach ($evals as $eval) {
+            $eval->setVisible($etat === 'visible');
+            $this->entityManager->persist($eval);
+        }
+
+        $this->entityManager->flush();
+
+        $this->addFlashBag(Constantes::FLASHBAG_SUCCESS, 'Modification de la visibilité des notes effectuées');
+
+        return $this->redirectToRoute('administration_notes_semestre_index', ['semestre' => $semestre->getId()]);
+    }
+
+    /**
+     * @param          $etat
+     * @param Semestre $semestre
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/modifiable/semestre/{semestre}/{etat}", name="administration_evaluation_modifiable_all",
+     *                                                  methods={"GET"})
+     */
+    public function modifiableAll(EvaluationRepository $evaluationRepository, $etat, Semestre $semestre)
+    {
+        $evals = $evaluationRepository->findBySemestre($semestre);
+
+        /** @var Evaluation $eval */
+        foreach ($evals as $eval) {
+            $eval->setModifiable($etat === 'autorise');
+            $this->entityManager->persist($eval);
+        }
+
+        $this->entityManager->flush();
+
+        $this->addFlashBag(Constantes::FLASHBAG_SUCCESS, 'Autorisation de modification des notes effectuée');
+
+        return $this->redirectToRoute('administration_notes_semestre_index', ['semestre' => $semestre->getId()]);
+    }
+
     /**
      *
      * @param Evaluation $evaluation
@@ -52,4 +103,6 @@ class EvaluationController extends BaseController
 
         return new JsonResponse($evaluation->getVisible(), Response::HTTP_OK);
     }
+
+
 }
---
+++ src/Controller/administration/ReleveController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\administration;
+
+use App\Controller\BaseController;
+use App\Entity\Semestre;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ReleveController
+ * @package App\Controller\administration
+ * @Route({"fr":"administration/note/releve",
+ *         "en":"administration/mark/releve"}
+ *)
+ */
+class ReleveController extends BaseController
+{
+    /**
+     * @param Semestre $semestre
+     * @Route("/provisoire/all/{semestre}", name="administration_note_all_releves_provisoires")
+     */
+    public function releveProvisoireAll(Semestre $semestre)
+    {
+
+    }
+}
