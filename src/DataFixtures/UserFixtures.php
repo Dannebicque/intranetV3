55a32a0a2e4e09d4e73f6f89578eb8e6b4494028
davidannebicque <david.annebicque@gmail.com>
2019-02-13 12:09:40 +0100

    Fixture User

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,20 +10,13 @@ use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
-    protected $encoder;
-
-    public function __construct(PasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
-
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
         $user = new Personnel();
         $user->setUsername('annebi01');
-        $user->setPassword($this->encoder->encodePassword('test', ''));
+        $user->setPassword('$2y$10$Nq3ZV0/lAQZCrUtxZZJn0OoIHGrhtn3JXgJwpJ7obD759XqLJF78y');
         $user->setMailUniv('david.annebicque@gmail.com');
         $user->setNom('Annebicque');
         $user->setPrenom('David');
