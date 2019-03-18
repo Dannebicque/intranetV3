5ed768d9353c61ec166e6241ebe9887757ce672a
Mathieu Piot <math.piot@gmail.com>
2019-03-18 21:10:14 +0100

    Install WebpackEncore bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
