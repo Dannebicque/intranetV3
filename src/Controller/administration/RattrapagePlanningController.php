ba569994e8945193b3a8f5b862962d386836336c
David Annebicque <david.annebicque@gmail.com>
2019-06-26 08:10:56 +0200

    Partie planning de rattrapage en admin

--- src/Controller/administration/RattrapagePlanningController.php
+++ src/Controller/administration/RattrapagePlanningController.php
@@ -22,6 +22,7 @@ use App\Entity\Rattrapage;
 use App\Entity\Semestre;
 use App\Events;
 use App\MesClasses\MyExport;
+use App\MesClasses\Tools;
 use App\Repository\AbsenceRepository;
 use App\Repository\RattrapageRepository;
 use DateTime;
@@ -59,11 +60,10 @@ class RattrapagePlanningController extends BaseController
     }
 
     /**
-     * @Route("/{semestre}/export.{_format}", name="administration_rattrapage_export", methods="GET",
+     * @Route("/{diplome}/export.{_format}", name="administration_rattrapage_planning_export", methods="GET",
      *                             requirements={"_format"="csv|xlsx|pdf"})
      * @param MyExport             $myExport
      * @param RattrapageRepository $rattrapageRepository
-     * @param Semestre             $semestre
      * @param                      $_format
      *
      * @return Response
@@ -72,7 +72,7 @@ class RattrapagePlanningController extends BaseController
     public function export(
         MyExport $myExport,
         RattrapageRepository $rattrapageRepository,
-        Semestre $semestre,
+        Diplome $diplome,
         $_format
     ): Response {
         $rattrapages = $rattrapageRepository->findBySemestre($semestre, $semestre->getAnneeUniversitaire());
@@ -99,20 +99,23 @@ class RattrapagePlanningController extends BaseController
     }
 
     /**
-     * @Route("/change/{uuid}/{type}/{data}", name="administration_rattrapage_planning_change", methods="GET",
-     *                                    requirements={"etat"="A|R"}, options={"expose":true})
+     * @Route("/change/{uuid}/{type}", name="administration_rattrapage_planning_change", methods="POST",
+     *                                    requirements={"type"="date|heure|salle"}, options={"expose":true})
      * @ParamConverter("rattrapage", options={"mapping": {"uuid": "uuid"}})
-     * @param EventDispatcherInterface $eventDispatcher
-     * @param Rattrapage               $rattrapage
-     * @param                          $etat
+     * @param Request    $request
+     * @param Rattrapage $rattrapage
+     *
+     * @param            $type
      *
      * @return Response
+     * @throws \Exception
      */
-    public function accepte(Rattrapage $rattrapage, $type, $data): Response
+    public function change( Request $request, Rattrapage $rattrapage, $type): Response
     {
+        $data = $request->request->get('data');
         switch ($type) {
             case 'date':
-                $rattrapage->setDateRattrapage(new DateTime($data));
+                $rattrapage->setDateRattrapage(Tools::convertDateToObject($data));
                 break;
             case 'heure':
                 $rattrapage->setHeureRattrapage(new DateTime($data));
@@ -122,6 +125,42 @@ class RattrapagePlanningController extends BaseController
                 break;
             default:
                 return new Response('', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        $this->entityManager->flush();
+        return $this->json(true, Response::HTTP_OK);
+
+    }
+
+    /**
+     * @Route("/update_global/{type}/{diplome}", name="administration_rattrapage_update_global", methods="POST",
+     *                                    requirements={"type"="salle|heure|date"}, options={"expose":true})
+     * @param EventDispatcherInterface $eventDispatcher
+     *
+     * @return Response
+     * @throws \Exception
+     */
+    public function updateGlobal(Request $request, RattrapageRepository $rattrapageRepository, $type, Diplome $diplome): Response
+    {
+        $valeur = $request->request->get('valeur');
+
+        $rattrapages = $rattrapageRepository->findValidByDiplome($diplome, $diplome->getAnneeUniversitaire());
+
+        /** @var Rattrapage $rattrapage */
+        foreach ($rattrapages as $rattrapage)
+        {
+            switch ($type)
+            {
+                case 'salle':
+                    $rattrapage->setSalle($valeur);
+                    break;
+                case 'heure':
+                    $rattrapage->setHeureRattrapage(new DateTime($valeur));
+                    break;
+                case 'date':
+                    $rattrapage->setDateRattrapage(Tools::convertDateToObject($valeur));
+                    break;
+            }
+            $this->entityManager->persist($rattrapage);
         }
         $this->entityManager->flush();
         return $this->json(true, Response::HTTP_OK);
