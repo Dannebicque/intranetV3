df06de7c64bf7cd2f065efcbfad14a40c61f7455
David Annebicque <david.annebicque@gmail.com>
2019-08-02 11:34:44 +0200

    INTRANET-3 configurer les logs de connexion sur 1an.

---
+++ src/Controller/superAdministration/ApogeeController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (C) 8 / 2019 | David annebicque | IUT de Troyes - All Rights Reserved
+ * @file /Users/davidannebicque/htdocs/intranetv3/src/Controller/superAdministration/ApogeeController.php
+ * @author     David Annebicque
+ * @project intranetv3
+ * @date 02/08/2019 11:34
+ * @lastUpdate 02/08/2019 11:34
+ */
+
+namespace App\Controller\superAdministration;
+
+use App\Controller\BaseController;
+use PDO;
+use PDOException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/super-administration/apogee")
+ */
+class ApogeeController extends BaseController
+{
+    /**
+     * @Route("/index", methods={"GET"}, name="sa_apogee_index")
+     * @IsGranted("ROLE_SUPER_ADMIN")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        try {
+            $conn = new PDO('oci:dbname=//pogee-db.univ-reims.fr:1522/APOGEE/ORCL', 'iut', 'Iut3Re1msApo!');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+
+        return $this->render('super-administration/apogee/index.html.twig', [
+
+        ]);
+    }
+}
