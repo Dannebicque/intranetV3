b0b7cdb1b36267f6a96729a2c006e701820b439c
David Annebicque <david.annebicque@gmail.com>
2019-08-26 13:44:04 +0200

    INTRANET-29 Imports etudiants depuis Apogée

--- src/Controller/superAdministration/ApogeeController.php
+++ src/Controller/superAdministration/ApogeeController.php
@@ -79,22 +79,28 @@ class ApogeeController extends BaseController
 
             while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
                 $dataApogee = $this->transformeApogeeToArray($row);
-                $etudiant = $etudiantRepository->findOneBy(['numEtudiant' => $dataApogee['setNumEtudiant']]);
+                $numEtudiant = $dataApogee['etudiant']['setNumEtudiant'];
+                $etudiant = $etudiantRepository->findOneBy(['numEtudiant' => $numEtudiant]);
                 if ($etudiant && $type === 'force') {
                     //todo: une classe ?
                     //on met à jour
-                    $etudiant->updateFromApogee($dataApogee);
+                    $etudiant->updateFromApogee($dataApogee['etudiant']);
+                    $etudiant->getAdresse()->updateFromApogee($dataApogee['adresse']);
                     $this->entityManager->flush();
-                    $etudiants[$dataApogee['setNumEtudiant']]['etat'] = 'maj';
-                    $etudiants[$dataApogee['setNumEtudiant']]['data'] = $etudiant;
+                    $etudiants[$numEtudiant]['etat'] = 'maj';
+                    $etudiants[$numEtudiant]['data'] = $etudiant;
                 } else {
                     //n'existe pas on ajoute.
                     $etudiant = new Etudiant();
-                    $etudiant->updateFromApogee($dataApogee);
+                    $etudiant->updateFromApogee($dataApogee['etudiant']);
+                    $adresse = new Adresse();
+                    $adresse->updateFromApogee($dataApogee['adresse']);
+                    $this->entityManager->persist($adresse);
+                    $etudiant->setAdresse($adresse);
                     $this->entityManager->persist($etudiant);
                     $this->entityManager->flush();
-                    $etudiants[$dataApogee['setNumEtudiant']]['etat'] = 'add';
-                    $etudiants[$dataApogee['setNumEtudiant']]['data'] = $etudiant;
+                    $etudiants[$numEtudiant]['etat'] = 'add';
+                    $etudiants[$numEtudiant]['data'] = $etudiant;
                 }
             }
             $this->addFlashBag('success', 'import.etudiant.apogee.ok');
@@ -124,33 +130,40 @@ class ApogeeController extends BaseController
         $listeetudiants = explode(';', $request->request->get('listeetudiants'));
         $etudiants = [];
         foreach ($listeetudiants as $etudiant) {
-            //            $conn = oci_connect($_ENV['APOGEE_LOGIN'], $_ENV['APOGEE_PASSWORD'], $_ENV['APOGEE_STRING']);
-//        if (!$conn) {
-//            $e = oci_error();
-//            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-//        }
-// todo: a définir
-//        $stid = oci_parse($conn, 'SELECT * FROM INS_ADM_ETP WHERE COD_ETP=\'5PSP13\'');
-//        oci_execute($stid);
-//
-//        while (row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
-            //requete pour récupérer les datas de l'étudiant et ajouter à la BDD.
-            $dataApogee = $this->transformeApogeeToArray($row);
-            //Stocker réponse dans un tableau pour page confirmation
-            $etudiant = $etudiantRepository->findOneBy(['numEtudiant' => $numEtudiant]);
-            if ($etudiant) {
-                $etudiants[$numEtudiant]['etat'] = 'deja';
-                $etudiants[$numEtudiant]['data'] = $etudiant;
-            } else {
-                //n'existe pas on ajoute.
-                $etudiant = new Etudiant();
-                $etudiant->updateFromApogee($dataApogee);
-                $this->entityManager->persist($etudiant);
-                $this->entityManager->flush();
-                $etudiants[$numEtudiant]['etat'] = 'add';
-                $etudiants[$numEtudiant]['data'] = $etudiant;
+            $conn = oci_connect($_ENV['APOGEE_LOGIN'], $_ENV['APOGEE_PASSWORD'], $_ENV['APOGEE_STRING']);
+            if (!$conn) {
+                $e = oci_error();
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
             }
-            //}
+
+            $stid = oci_parse($conn,
+                'SELECT * FROM INS_ADM_ETP INNER JOIN INDIVIDU ON INDIVIDU.COD_IND = INS_ADM_ETP.COD_IND INNER JOIN ADRESSE ON ADRESSE.COD_IND = INS_ADM_ETP.COD_IND WHERE INDIVIDU.COD_IND=\'' . $etudiant . '\'');
+            oci_execute($stid);
+
+            while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+                //requete pour récupérer les datas de l'étudiant et ajouter à la BDD.
+                $dataApogee = $this->transformeApogeeToArray($row);
+                $numEtudiant = $dataApogee['etudiant']['setNumEtudiant'];
+
+                //Stocker réponse dans un tableau pour page confirmation
+                $etudiant = $etudiantRepository->findOneBy(['numEtudiant' => $numEtudiant]);
+                if ($etudiant) {
+                    $etudiants[$numEtudiant]['etat'] = 'deja';
+                    $etudiants[$numEtudiant]['data'] = $etudiant;
+                } else {
+                    //n'existe pas on ajoute.
+                    $etudiant = new Etudiant();
+                    $etudiant->updateFromApogee($dataApogee['etudiant']);
+                    $adresse = new Adresse();
+                    $adresse->updateFromApogee($dataApogee['adresse']);
+                    $this->entityManager->persist($adresse);
+                    $etudiant->setAdresse($adresse);
+                    $this->entityManager->persist($etudiant);
+                    $this->entityManager->flush();
+                    $etudiants[$numEtudiant]['etat'] = 'add';
+                    $etudiants[$numEtudiant]['data'] = $etudiant;
+                }
+            }
         }
         $this->addFlashBag('success', 'import.etudiant.apogee.ok');
 
@@ -159,17 +172,34 @@ class ApogeeController extends BaseController
         ]);
     }
 
-    private function transformeApogeeToArray($data)
+    /**
+     * @param $data
+     *
+     * @return array
+     */
+    private function transformeApogeeToArray($data): array
     {
+        // COD_ETU, COD_NNE_IND, DATE_NAI_IND, DAA_ENT_ETB, LIB_NOM_PAT_IND, LIB_PR1_IND, COD_SEX_ETU
         return [
-            'setNumEtudiant'   => $data['COD_ETU'],
-            'setNumIne'        => $data['COD_NNE_IND'],
-            'setDateNaissance' => Tools::convertDateToObject($data['DATE_NAI_IND']), //en fr?
-            'setPromotion'     => $data['DAA_ENT_ETB'],
-            'setNom'           => $data['LIB_NOM_PAT_IND'],
-            'setPrenom'        => $data['LIB_PR1_IND'],
-            'setCivilite'      => $data['COD_SEX_ETU'] === 'M' ? 'M.' : 'Mme', //M ou F
-            'setTypeUser'      => 'etudiant',
+            'etudiant' => [
+                'setNumEtudiant'   => $data['COD_ETU'],
+                'setNumIne'        => $data['COD_NNE_IND'],
+                'setDateNaissance' => Tools::convertDateToObject($data['DATE_NAI_IND']), //en fr?
+                'setPromotion'     => $data['DAA_ENT_ETB'],
+                'setNom'           => $data['LIB_NOM_PAT_IND'],
+                'setPrenom'        => $data['LIB_PR1_IND'],
+                'setTel1'          => $data['NUM_TEL'],
+                'setCivilite'      => $data['COD_SEX_ETU'] === 'M' ? 'M.' : 'Mme', //M ou F
+                'setTypeUser'      => 'etudiant',
+            ],
+            'adresse'  => [
+                'setAdresse1'   => $data['LIB_AD1'],
+                'setAdresse2'   => $data['LIB_AD2'],
+                'setAdresse3'   => $data['LIB_AD3'],
+                'setCodePostal' => $data['COD_BDI'],
+                'setVille'      => $data['COD_COM'], //code commune INSEE
+                'setPays'       => $data['COD_PAY '], //code
+            ]
         ];
     }
 
