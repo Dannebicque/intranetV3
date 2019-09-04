f703968f6cb44cbdbac27e20827437a106ab8d9c
David Annebicque <david.annebicque@gmail.com>
2019-09-04 16:58:35 +0200

    [Structure] : Possibilité de Marquer une UE en "bonification"

--- src/Entity/Ue.php
+++ src/Entity/Ue.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Copyright (C) 8 / 2019 | David annebicque | IUT de Troyes - All Rights Reserved
+ * Copyright (C) 9 / 2019 | David annebicque | IUT de Troyes - All Rights Reserved
  * @file /Users/davidannebicque/htdocs/intranetv3/src/Entity/Ue.php
  * @author     David Annebicque
  * @project intranetv3
- * @date 21/08/2019 12:29
- * @lastUpdate 21/08/2019 12:15
+ * @date 04/09/2019 16:58
+ * @lastUpdate 04/09/2019 16:56
  */
 
 namespace App\Entity;
@@ -73,6 +73,11 @@ class Ue extends BaseEntity
      * @ORM\Column(type="boolean")
      */
     private $actif = true;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $bonification;
 
     /**
      * Ue constructor.
@@ -301,5 +306,17 @@ class Ue extends BaseEntity
     public function getDisplay(): string
     {
         return 'UE '.$this->getNumeroUe().' | '.$this->getLibelle();
+    }
+
+    public function getBonification(): ?bool
+    {
+        return $this->bonification;
+    }
+
+    public function setBonification(bool $bonification): self
+    {
+        $this->bonification = $bonification;
+
+        return $this;
     }
 }
