414fbcdcef2e2a1799e5258ee0f944651e1c9119
David Annebicque <david.annebicque@gmail.com>
2019-09-21 11:51:53 +0200

    INTRANET-2 Intégrer double authentification CAS + Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Copyright (C) 9 / 2019 | David annebicque | IUT de Troyes - All Rights Reserved
+ * @file /Users/davidannebicque/htdocs/intranetv3/config/bundles.php
+ * @author     David Annebicque
+ * @project intranetv3
+ * @date 21/09/2019 11:51
+ * @lastUpdate 21/09/2019 11:51
+ */
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
@@ -12,11 +20,12 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
-    L3\Bundle\CasBundle\L3CasBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class            => ['all' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class              => ['all' => true],
+    L3\Bundle\CasBundle\L3CasBundle::class                       => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class                => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class    => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Translation\Bundle\TranslationBundle::class => ['all' => true],
+    Translation\Bundle\TranslationBundle::class                  => ['all' => true],
+    PRayno\CasAuthBundle\PRaynoCasAuthBundle::class              => ['all' => true],
 ];
---
+++ src/Security/CasAuthenticator.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright (C) 9 / 2019 | David annebicque | IUT de Troyes - All Rights Reserved
+ * @file /Users/davidannebicque/htdocs/intranetv3/src/Security/CasAuthenticator.php
+ * @author     David Annebicque
+ * @project intranetv3
+ * @date 21/09/2019 11:51
+ * @lastUpdate 21/09/2019 11:15
+ */
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class CasAuthenticator extends AbstractGuardAuthenticator
+{
+    public function supports(Request $request)
+    {
+        return 'security_login' === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        // todo
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // todo
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // todo
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // todo
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}
