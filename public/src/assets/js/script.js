27267f2d4b2aa7c43341d6014468b96206e60c7b
David Annebicque <david.annebicque@gmail.com>
2019-09-21 11:54:32 +0200

    INTRANET-2 Intégrer double authentification CAS + Symfony

--- public/src/assets/js/script.js
+++ public/src/assets/js/script.js
@@ -2,8 +2,8 @@
 // @file /Users/davidannebicque/htdocs/intranetv3/public/src/assets/js/script.js
 // @author     David Annebicque
 // @project intranetv3
-// @date 21/09/2019 11:53
-// @lastUpdate 21/09/2019 09:14
+// @date 21/09/2019 11:54
+// @lastUpdate 21/09/2019 11:53
 
 'use strict'
 
