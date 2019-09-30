280b2ec14e6e35eaddb018360bc994601ec2e534
David Annebicque <david.annebicque@gmail.com>
2019-09-30 10:36:04 +0200

    INTRANET-2 Intégrer double authentification CAS + Symfony

--- src/Security/CasAuthenticator.php
+++ src/Security/CasAuthenticator.php
@@ -4,19 +4,27 @@
  * @file /Users/davidannebicque/htdocs/intranetv3/src/Security/CasAuthenticator.php
  * @author     David Annebicque
  * @project intranetv3
- * @date 30/09/2019 10:24
- * @lastUpdate 30/09/2019 10:24
+ * @date 30/09/2019 10:36
+ * @lastUpdate 30/09/2019 10:35
  */
 
 namespace App\Security;
 
+use App\Entity\Departement;
 use App\Event\CASAuthenticationFailureEvent;
+use App\Events;
+use App\Repository\DepartementRepository;
+use App\Repository\EtudiantRepository;
+use App\Repository\PersonnelRepository;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -27,16 +35,39 @@ class CasAuthenticator extends AbstractGuardAuthenticator
     protected $server_login_url = 'https://cas.univ-reims.fr/cas/';
     protected $options;
     private $eventDispatcher;
+    private $etudiantRepository;
+    private $personnelRepository;
+    private $departementRepository;
+    private $user;
+    private $urlGenerator;
+    private $session;
+
 
     /**
      * Process configuration
      *
-     * @param array $config
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param EtudiantRepository       $etudiantRepository
+     * @param PersonnelRepository      $personnelRepository
+     * @param UrlGeneratorInterface    $urlGenerator
+     * @param DepartementRepository    $departementRepository
+     * @param SessionInterface         $session
      */
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        EtudiantRepository $etudiantRepository,
+        PersonnelRepository $personnelRepository,
+        UrlGeneratorInterface $urlGenerator,
+        DepartementRepository $departementRepository,
+        SessionInterface $session
+    ) {
 
         $this->eventDispatcher = $eventDispatcher;
+        $this->personnelRepository = $personnelRepository;
+        $this->etudiantRepository = $etudiantRepository;
+        $this->urlGenerator = $urlGenerator;
+        $this->departementRepository = $departementRepository;
+        $this->session = $session;
     }
 
     public function supports(Request $request)
@@ -75,7 +106,25 @@ class CasAuthenticator extends AbstractGuardAuthenticator
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        $userPersonnel = $this->personnelRepository->findOneBy(['username' => $credentials['username']]);
+        $userEtudiant = $this->etudiantRepository->findOneBy(['username' => $credentials['username']]);
+
+        if (!$userPersonnel && !$userEtudiant) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+        }
+
+        if ($userPersonnel !== null && $userEtudiant === null) {
+            $this->user = $userPersonnel;
+
+            return $userPersonnel;
+        }
+
+        if ($userPersonnel === null && $userEtudiant !== null) {
+            $this->user = $userEtudiant;
+
+            return $userEtudiant;
+        }
     }
 
     public function checkCredentials($credentials, UserInterface $user)
@@ -100,16 +149,69 @@ class CasAuthenticator extends AbstractGuardAuthenticator
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         dump('authentifié');
+        dump('ok');
+        dump($this->removeCasTicket($request->getUri()));
+
+        $rolesTab = $token->getRoleNames();
+
+        if (in_array('ROLE_SUPER_ADMIN', $rolesTab, true)) {
+            // c'est un super administrateur : on le rediriger vers l'espace super-admin
+            $redirection = new RedirectResponse($this->urlGenerator->generate('super_admin_homepage'));
+        } elseif (in_array('ROLE_ADMINISTRATIF', $rolesTab, true)) {
+            // c'est un administratif : on le rediriger vers l'espace administration
+            $redirection = new RedirectResponse($this->urlGenerator->generate('administratif_homepage'));
+        } elseif (in_array('ROLE_PERMANENT', $rolesTab, true) || in_array('ROLE_ETUDIANT', $rolesTab, true)) {
+            // c'est un utilisaeur étudiant ou prof : on le rediriger vers l'accueil
+
+            if (in_array('ROLE_PERMANENT', $rolesTab, true)) {
+                //init de la session departement
+                $departements = $this->departementRepository->findDepartementPersonnelDefaut($this->user);
+                if (count($departements) > 1) {
+                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+                }
+
+                if (count($departements) === 1) {
+                    /** @var Departement $departement */
+                    $departement = $departements[0];
+                    $this->session->set('departement', $departement->getUuidString()); //on sauvegarde
+                } else {
+                    echo 'pas de departement par defaut';
+                    //pas de departement par défaut, ou pas de departement du tout.
+                    $departements = $this->departementRepository->findDepartementPersonnel($this->user);
+                    if (count($departements) === 0) {
+                        return new RedirectResponse($this->urlGenerator->generate('security_login',
+                            ['events' => Events::REDIRECT_TO_LOGIN, 'message' => 'pas-departement']));
+                    }
+
+                    //donc il y a une departement, mais pas une par défaut.
+                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+                }
+            }
+
+//            if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+//                return new RedirectResponse($targetPath);
+//            }
+
+            $redirection = new RedirectResponse($this->urlGenerator->generate('default_homepage'));
+        } else {
+            //c'est aucun des rôles...
+            $redirection = new RedirectResponse($this->urlGenerator->generate('security_login',
+                ['message' => 'erreur_role']));
+        }
+
+        return $redirection;
+
+
         // todo
         // If authentication was successful, redirect to the current URI with
         // the ticket parameter removed so that it is hidden from end-users.
-        if ($request->query->has($this->ticket)) {
-            dump('ok');
-            dump($this->removeCasTicket($request->getUri()));
-            //return new RedirectResponse($this->removeCasTicket($request->getUri()));
-        } else {
-            return null;
-        }
+//        if ($request->query->has($this->ticket)) {
+//            dump('ok');
+//            dump($this->removeCasTicket($request->getUri()));
+//            //return new RedirectResponse($this->removeCasTicket($request->getUri()));
+//        } else {
+//            return null;
+//        }
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
