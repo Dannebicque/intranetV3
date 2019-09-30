86c370fbbaaa380862b6b74f9acea93185cff2d7
David Annebicque <david.annebicque@gmail.com>
2019-09-30 10:59:49 +0200

    INTRANET-2 Intégrer double authentification CAS + Symfony

--- src/Security/CasAuthenticator.php
+++ src/Security/CasAuthenticator.php
@@ -4,15 +4,13 @@
  * @file /Users/davidannebicque/htdocs/intranetv3/src/Security/CasAuthenticator.php
  * @author     David Annebicque
  * @project intranetv3
- * @date 30/09/2019 10:53
- * @lastUpdate 30/09/2019 10:53
+ * @date 30/09/2019 10:59
+ * @lastUpdate 30/09/2019 10:59
  */
 
 namespace App\Security;
 
-use App\Entity\Departement;
 use App\Event\CASAuthenticationFailureEvent;
-use App\Events;
 use App\Repository\DepartementRepository;
 use App\Repository\EtudiantRepository;
 use App\Repository\PersonnelRepository;
@@ -153,61 +151,60 @@ class CasAuthenticator extends AbstractGuardAuthenticator
             $token->setAttributes(phpCAS::getAttributes());
         }
 
-        dump('authentifié');
-        dump('ok');
-        dump($this->removeCasTicket($request->getUri()));
-
-        $rolesTab = $token->getRoleNames();
-
-        if (in_array('ROLE_SUPER_ADMIN', $rolesTab, true)) {
-            // c'est un super administrateur : on le rediriger vers l'espace super-admin
-            $redirection = new RedirectResponse($this->urlGenerator->generate('super_admin_homepage'));
-        } elseif (in_array('ROLE_ADMINISTRATIF', $rolesTab, true)) {
-            // c'est un administratif : on le rediriger vers l'espace administration
-            $redirection = new RedirectResponse($this->urlGenerator->generate('administratif_homepage'));
-        } elseif (in_array('ROLE_PERMANENT', $rolesTab, true) || in_array('ROLE_ETUDIANT', $rolesTab, true)) {
-            // c'est un utilisaeur étudiant ou prof : on le rediriger vers l'accueil
-
-            if (in_array('ROLE_PERMANENT', $rolesTab, true)) {
-                //init de la session departement
-                $departements = $this->departementRepository->findDepartementPersonnelDefaut($this->user);
-                if (count($departements) > 1) {
-                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
-                }
-
-                if (count($departements) === 1) {
-                    /** @var Departement $departement */
-                    $departement = $departements[0];
-                    $this->session->set('departement', $departement->getUuidString()); //on sauvegarde
-                } else {
-                    echo 'pas de departement par defaut';
-                    //pas de departement par défaut, ou pas de departement du tout.
-                    $departements = $this->departementRepository->findDepartementPersonnel($this->user);
-                    if (count($departements) === 0) {
-                        return new RedirectResponse($this->urlGenerator->generate('security_login',
-                            ['events' => Events::REDIRECT_TO_LOGIN, 'message' => 'pas-departement']));
-                    }
-
-                    //donc il y a une departement, mais pas une par défaut.
-                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
-                }
-            }
-
-//            if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-//                return new RedirectResponse($targetPath);
+//        dump('authentifié');
+//        dump('ok');
+//        dump($this->removeCasTicket($request->getUri()));
+//
+//        $rolesTab = $token->getRoleNames();
+//
+//        if (in_array('ROLE_SUPER_ADMIN', $rolesTab, true)) {
+//            // c'est un super administrateur : on le rediriger vers l'espace super-admin
+//            $redirection = new RedirectResponse($this->urlGenerator->generate('super_admin_homepage'));
+//        } elseif (in_array('ROLE_ADMINISTRATIF', $rolesTab, true)) {
+//            // c'est un administratif : on le rediriger vers l'espace administration
+//            $redirection = new RedirectResponse($this->urlGenerator->generate('administratif_homepage'));
+//        } elseif (in_array('ROLE_PERMANENT', $rolesTab, true) || in_array('ROLE_ETUDIANT', $rolesTab, true)) {
+//            // c'est un utilisaeur étudiant ou prof : on le rediriger vers l'accueil
+//
+//            if (in_array('ROLE_PERMANENT', $rolesTab, true)) {
+//                //init de la session departement
+//                $departements = $this->departementRepository->findDepartementPersonnelDefaut($this->user);
+//                if (count($departements) > 1) {
+//                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+//                }
+//
+//                if (count($departements) === 1) {
+//                    /** @var Departement $departement */
+//                    $departement = $departements[0];
+//                    $this->session->set('departement', $departement->getUuidString()); //on sauvegarde
+//                } else {
+//                    echo 'pas de departement par defaut';
+//                    //pas de departement par défaut, ou pas de departement du tout.
+//                    $departements = $this->departementRepository->findDepartementPersonnel($this->user);
+//                    if (count($departements) === 0) {
+//                        return new RedirectResponse($this->urlGenerator->generate('security_login',
+//                            ['events' => Events::REDIRECT_TO_LOGIN, 'message' => 'pas-departement']));
+//                    }
+//
+//                    //donc il y a une departement, mais pas une par défaut.
+//                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+//                }
 //            }
+//
+////            if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+////                return new RedirectResponse($targetPath);
+////            }
+//
+//            $redirection = new RedirectResponse($this->urlGenerator->generate('default_homepage'));
+//        } else {
+//            //c'est aucun des rôles...
+//            $redirection = new RedirectResponse($this->urlGenerator->generate('security_login',
+//                ['message' => 'erreur_role']));
+//        }
+//
+//        return $redirection;
 
-            $redirection = new RedirectResponse($this->urlGenerator->generate('default_homepage'));
-        } else {
-            //c'est aucun des rôles...
-            $redirection = new RedirectResponse($this->urlGenerator->generate('security_login',
-                ['message' => 'erreur_role']));
-        }
 
-        return $redirection;
-
-
-        // todo
         // If authentication was successful, redirect to the current URI with
         // the ticket parameter removed so that it is hidden from end-users.
 //        if ($request->query->has($this->ticket)) {
@@ -261,5 +258,10 @@ class CasAuthenticator extends AbstractGuardAuthenticator
         $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
 
         return "$scheme$user$pass$host$port$path$query$fragment";
+    }
+
+    protected function getDefaultSuccessRedirectUrl()
+    {
+        return $this->urlGenerator->generate('default_homepage');
     }
 }
