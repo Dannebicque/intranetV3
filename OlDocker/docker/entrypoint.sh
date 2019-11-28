a5ab4da3d77f40c3563bdb51798158fb7e61fb25
David Annebicque <david.annebicque@gmail.com>
2019-11-28 20:04:46 +0100

    suppression du répertoire assets

--- OlDocker/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-bin/console doctrine:migration:migrate -n
-bin/console cache:clear --no-warmup
-bin/console cache:warmup
-
-exec "$@"
--- assets/js/plugins/pagination.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.page-link', function (e) {
-  e.preventDefault();
-  e.stopPropagation();
-  var zone = $('#zone-pagination');
-  zone.empty();
-  zone.load($(this).attr('href'));
-});
