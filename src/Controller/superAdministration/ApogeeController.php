5b3b7e811d3455109ed0a07cceefa30c1e8a94fe
David Annebicque <david.annebicque@gmail.com>
2019-12-09 19:13:38 +0100

    Apogee.

--- src/Controller/superAdministration/ApogeeController.php
+++ src/Controller/superAdministration/ApogeeController.php
@@ -163,6 +163,7 @@ class ApogeeController extends BaseController
         echo '<h3>requête de test de LDAP</h3>';
         echo 'Connexion ...';
         $ds=ldap_connect("ldap://ldap.univ-reims.fr");  // doit être un serveur LDAP valide !
+        ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
         echo 'Le résultat de connexion est ' . $ds . '<br />';
 
         if ($ds) {
