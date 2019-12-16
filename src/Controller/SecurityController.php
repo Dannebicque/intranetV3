c09704a357753d012366bc5ee66bd96a92972b6f
David Annebicque <david.annebicque@gmail.com>
2019-12-16 22:54:23 +0100

    CAS + Double authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -39,7 +39,7 @@ class SecurityController extends AbstractController
     public function redirectToCas()
     {
         //TODO : ADD CAS REDIRECT
-        return $this->redirect('https://cas.univ-reims.fr/cas?service='.$this->generateUrl('default_homepage'));
+        return $this->redirect('https://cas.univ-reims.fr/cas?service='.$this->generateUrl('default_homepage', [], UrlGeneratorInterface::ABSOLUTE_URL));
     }
 
     /**
