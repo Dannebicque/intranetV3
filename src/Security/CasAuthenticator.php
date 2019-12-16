2d4eac4172aeacca0fb0e432d59f01798e08af83
David Annebicque <david.annebicque@gmail.com>
2019-12-16 23:03:53 +0100

    CAS + Double authentification

--- src/Security/CasAuthenticator.php
+++ src/Security/CasAuthenticator.php
@@ -8,29 +8,43 @@
 
 namespace App\Security;
 
+use App\Entity\Departement;
 use App\Event\CASAuthenticationFailureEvent;
+use App\Events;
+use App\Repository\DepartementRepository;
 use phpCAS;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class CasAuthenticator extends AbstractGuardAuthenticator
 {
     private $urlGenerator;
     private $user;
+    private $session;
+
+    /** @var DepartementRepository */
+    private $departementRepository;
 
     public function __construct(
-        UrlGeneratorInterface $urlGenerator
+        UrlGeneratorInterface $urlGenerator,
+        DepartementRepository $departementRepository,
+        SessionInterface $session
+
     ) {
         $this->urlGenerator = $urlGenerator;
-
+        $this->departementRepository = $departementRepository;
+        $this->session = $session;
     }
 
     public function supports(Request $request): bool
@@ -99,11 +113,49 @@ class CasAuthenticator extends AbstractGuardAuthenticator
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        //if (phpCAS::isInitialized()) {
-            return new RedirectResponse($this->urlGenerator->generate('default_homepage'));
-        //}
+        $rolesTab = $token->getRoleNames();
+        if (in_array('ROLE_SUPER_ADMIN', $rolesTab, true) || in_array('ROLE_ADMINISTRATIF', $rolesTab, true)) {
+            // c'est un super administrateur : on le rediriger vers l'espace super-admin
+            $redirection = new RedirectResponse($this->urlGenerator->generate('super_admin_homepage'));
+        } elseif (in_array('ROLE_PERMANENT', $rolesTab, true) || in_array('ROLE_ETUDIANT', $rolesTab, true)) {
+            // c'est un utilisaeur étudiant ou prof : on le rediriger vers l'accueil
 
-        //return null;
+            if (in_array('ROLE_PERMANENT', $rolesTab, true)) {
+                //init de la session departement
+                $departements = $this->departementRepository->findDepartementPersonnelDefaut($this->user);
+                if (count($departements) > 1) {
+                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+                }
+
+                if (count($departements) === 1) {
+                    /** @var Departement $departement */
+                    $departement = $departements[0];
+                    $this->session->set('departement', $departement->getUuidString()); //on sauvegarde
+                } else {
+                    echo 'pas de departement par defaut';
+                    //pas de departement par défaut, ou pas de departement du tout.
+                    $departements = $this->departementRepository->findDepartementPersonnel($this->user);
+                    if (count($departements) === 0) {
+                        return new RedirectResponse($this->urlGenerator->generate('security_login',
+                            ['events' => Events::REDIRECT_TO_LOGIN, 'message' => 'pas-departement']));
+                    }
+                    //donc il y a une departement, mais pas une par défaut.
+                    return new RedirectResponse($this->urlGenerator->generate('security_choix_departement'));
+                }
+            }
+
+            if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+                return new RedirectResponse($targetPath);
+            }
+
+            $redirection = new RedirectResponse($this->urlGenerator->generate('default_homepage'));
+        } else {
+            //c'est aucun des rôles...
+            $redirection = new RedirectResponse($this->urlGenerator->generate('security_login',
+                ['message' => 'erreur_role']));
+        }
+
+        return $redirection;
     }
 
     public function supportsRememberMe(): bool
