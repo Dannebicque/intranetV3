20731a94d2a011ccf154549c03eec6fb2042f537
David Annebicque <david.annebicque@gmail.com>
2019-12-17 17:17:01 +0100

    Partie qualité

--- src/Controller/api/AlexaApiController.php
+++ src/Controller/api/AlexaApiController.php
@@ -10,6 +10,7 @@ namespace App\Controller\api;
 
 use App\Controller\BaseController;
 use App\Entity\TypeGroupe;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,11 +26,12 @@ class AlexaApiController extends BaseController
      *                                        options={"expose":true})
      *
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function tests(): Response
+    public function tests(Request $request): Response
     {
-        $tab = ['coucou' => 'test'];
-        return $this->json($tab);
+        return $this->json($request);
     }
 }
