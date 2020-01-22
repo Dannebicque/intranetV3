a66d0f34d909c62abd348c361f1929274af0fd39
David Annebicque <david.annebicque@gmail.com>
2020-01-22 18:22:22 +0100

    Correctifs partie absence

--- src/Controller/appPersonnel/AbsenceController.php
+++ src/Controller/appPersonnel/AbsenceController.php
@@ -177,7 +177,7 @@ class AbsenceController extends BaseController
             return $this->json($id, Response::HTTP_OK);
         }
 
-        $this->addFlashBag(Constantes::FLASHBAG_ERROR, 'date.delete.error.flash');
+        $this->addFlashBag(Constantes::FLASHBAG_ERROR, 'absence.delete.error.flash');
 
         return $this->json(false, Response::HTTP_INTERNAL_SERVER_ERROR);
     }
