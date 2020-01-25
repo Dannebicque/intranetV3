61fec4a3a5d0982041821a73b5f760691a449c96
David Annebicque <david.annebicque@gmail.com>
2020-01-25 22:20:03 +0100

    Bug Parcours

---
+++ src/Command/UpdateEdtCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class UpdateEdtCommand extends Command
+{
+    protected static $defaultName = 'app:update-edt';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Synchronisation de l\'emploi du temps avec Celcat')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
