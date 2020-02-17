cb1fa6287b71d88ffbe138e55a963bcc05868bbb
David Annebicque <david.annebicque@gmail.com>
2020-02-17 07:37:38 +0100

    Correctifs fiches de suivi des alternants en Admin

--- src/Controller/administration/AlternanceFicheSuiviController.php
+++ src/Controller/administration/AlternanceFicheSuiviController.php
@@ -8,9 +8,12 @@
 
 namespace App\Controller\administration;
 
+use App\Entity\Alternance;
 use App\Entity\AlternanceFicheSuivi;
 use App\Form\AlternanceFicheSuiviType;
 use App\Repository\AlternanceFicheSuiviRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,33 +21,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/alternance/fiche/suivi")
+ * @Route("/administration/alternance/fiche/suivi")
  */
 class AlternanceFicheSuiviController extends AbstractController
 {
     /**
-     * @Route("/", name="alternance_fiche_suivi_index", methods={"GET"})
-     * @param AlternanceFicheSuiviRepository $alternanceFicheSuiviRepository
-     *
-     * @return Response
-     */
-    public function index(AlternanceFicheSuiviRepository $alternanceFicheSuiviRepository): Response
-    {
-        return $this->render('administration/alternance_fiche_suivi/index.html.twig', [
-            'alternance_fiche_suivis' => $alternanceFicheSuiviRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="alternance_fiche_suivi_new", methods={"GET","POST"})
+     * @Route("/new/{alternance}", name="administration_alternance_fiche_suivi_new", methods={"GET","POST"})
      * @param Request $request
      *
      * @return Response
      * @throws Exception
      */
-    public function new(Request $request): Response
+    public function new(Request $request, Alternance $alternance): Response
     {
-        $alternanceFicheSuivi = new AlternanceFicheSuivi();
+        $alternanceFicheSuivi = new AlternanceFicheSuivi($alternance);
         $form = $this->createForm(AlternanceFicheSuiviType::class, $alternanceFicheSuivi);
         $form->handleRequest($request);
 
@@ -53,7 +43,7 @@ class AlternanceFicheSuiviController extends AbstractController
             $entityManager->persist($alternanceFicheSuivi);
             $entityManager->flush();
 
-            return $this->redirectToRoute('alternance_fiche_suivi_index');
+            return $this->redirectToRoute('administration_alternance_show', ['alternance' => $alternance->getId()]);
         }
 
         return $this->render('administration/alternance_fiche_suivi/new.html.twig', [
@@ -63,7 +53,32 @@ class AlternanceFicheSuiviController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="alternance_fiche_suivi_show", methods={"GET"})
+     * @Route("/imprimer/{id}", name="administration_alternance_fiche_suivi_export", methods={"GET"})
+     * @param AlternanceFicheSuivi $alternanceFicheSuivi
+     *
+     * @return Response
+     */
+    public function print(AlternanceFicheSuivi $alternanceFicheSuivi): Response
+    {
+        $html = $this->renderView('pdf/ficheSuiviAlternant.html.twig', [
+            'alternance_fiche_suivi' => $alternanceFicheSuivi,
+        ]);
+
+        $options = new Options();
+        $options->set('isRemoteEnabled', true);
+        $options->set('isPhpEnabled', true);
+
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+
+        return new Response($dompdf->stream('Fiche-suivi-alternant-' . $alternanceFicheSuivi->getAlternance()->getEtudiant()->getNom() . '-' . $alternanceFicheSuivi->getDate()->format('dmY'),
+            ['Attachment' => 1]));
+    }
+
+
+    /**
+     * @Route("/{id}", name="administration_alternance_fiche_suivi_show", methods={"GET"})
      * @param AlternanceFicheSuivi $alternanceFicheSuivi
      *
      * @return Response
@@ -76,7 +91,7 @@ class AlternanceFicheSuiviController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="alternance_fiche_suivi_edit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="administration_alternance_fiche_suivi_edit", methods={"GET","POST"})
      * @param Request              $request
      * @param AlternanceFicheSuivi $alternanceFicheSuivi
      *
@@ -90,9 +105,15 @@ class AlternanceFicheSuiviController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('alternance_fiche_suivi_index', [
-                'id' => $alternanceFicheSuivi->getId(),
-            ]);
+            if ($request->request->get('btn_update') !== null) {
+                return $this->redirectToRoute('administration_alternance_show',
+                    ['alternance' => $alternanceFicheSuivi->getAlternance()->getId()]);
+            }
+
+            return $this->redirectToRoute('administration_alternance_fiche_suivi_edit',
+                ['id' => $alternanceFicheSuivi->getId()]);
+
+
         }
 
         return $this->render('administration/alternance_fiche_suivi/edit.html.twig', [
@@ -102,7 +123,7 @@ class AlternanceFicheSuiviController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="alternance_fiche_suivi_delete", methods={"DELETE"})
+     * @Route("/{id}", name="administration_alternance_fiche_suivi_delete", methods={"DELETE"})
      * @param Request              $request
      * @param AlternanceFicheSuivi $alternanceFicheSuivi
      *
@@ -111,11 +132,12 @@ class AlternanceFicheSuiviController extends AbstractController
     public function delete(Request $request, AlternanceFicheSuivi $alternanceFicheSuivi): Response
     {
         if ($this->isCsrfTokenValid('delete' . $alternanceFicheSuivi->getId(), $request->request->get('_token'))) {
+            $alternance = $alternanceFicheSuivi->getAlternance();
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($alternanceFicheSuivi);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('alternance_fiche_suivi_index');
+        return $this->redirectToRoute('administration_alternance_show', ['alternance' => $alternance->getId()]);
     }
 }
--- src/DataFixtures/AnneeUniversitaireFixture.php
+++ src/DataFixtures/AnneeUniversitaireFixture.php
@@ -11,7 +11,7 @@ namespace App\DataFixtures;
 use App\Entity\AnneeUniversitaire;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class AnneeUniversitaireFixture extends Fixture
 {
