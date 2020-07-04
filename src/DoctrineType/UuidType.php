f7684a6afaa776bdb275602b4b92e9c60eca81e6
David Annebicque <david.annebicque@gmail.com>
2020-07-04 09:59:33 +0200

    Gestion département et UID

--- src/DoctrineType/UuidType.php
+++ src/DoctrineType/UuidType.php
@@ -1,93 +1,59 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DoctrineType;
 
-use InvalidArgumentException;
-use Symfony\Component\Uid\Uuid;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\GuidType;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Symfony\Component\Uid\UuidV4;
+use Symfony\Component\Uid\Uuid;
+use Throwable;
 
 /**
- * Field type mapping for the Doctrine Database Abstraction Layer (DBAL).
- *
- * UUID fields will be stored as a string in the database and converted back to
- * the Uuid value object when querying.
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
  */
-class UuidType extends GuidType
+final class UuidType extends GuidType
 {
-    /**
-     * @var string
-     */
-    const NAME = 'uuid';
+    private const NAME = 'uuid';
 
     /**
      * {@inheritdoc}
-     *
-     * @param string|UuidV4|null $value
-     * @param AbstractPlatform   $platform
-     *
-     * @return UuidV4|null
-     *
-     * @throws ConversionException
-     */
-    public function convertToPHPValue($value, AbstractPlatform $platform)
-    {
-        if ($value === null || $value === '') {
-            return null;
-        }
-
-        if ($value instanceof UuidV4) {
-            return $value;
-        }
-
-        try {
-            $uuid = UuidV4::fromString($value);
-        } catch (InvalidArgumentException $e) {
-            throw ConversionException::conversionFailed($value, static::NAME);
-        }
-
-        return $uuid;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @param UuidV4|string|null $value
-     * @param AbstractPlatform   $platform
-     *
-     * @return string|null
-     *
-     * @throws ConversionException
      */
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
-        if ($value === null || $value === '') {
+        if (null === $value || '' === $value) {
             return null;
         }
 
-        if (
-            $value instanceof UuidV4
-            || (
-                (is_string($value)
-                    || method_exists($value, '__toString'))
-                && Uuid::isValid((string)$value)
-            )
-        ) {
-            return (string)$value;
+        if ($value instanceof Uuid) {
+            return $value->toRfc4122();
         }
 
-        throw ConversionException::conversionFailed($value, static::NAME);
+        throw ConversionException::conversionFailed($value, self::NAME);
     }
 
     /**
      * {@inheritdoc}
-     *
-     * @return string
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || '' === $value) {
+            return null;
+        }
+
+        if (!Uuid::isValid($value)) {
+            throw ConversionException::conversionFailed($value, self::NAME);
+        }
+
+        return Uuid::fromString($value);
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function getName()
     {
-        return static::NAME;
+        return self::NAME;
     }
 }
