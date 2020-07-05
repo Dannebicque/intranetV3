3b7ae125691c9f5a18c41451833aa073f1cf7483
David Annebicque <david.annebicque@gmail.com>
2020-07-05 19:16:01 +0200

    Passage à Webpack + datatables

---
+++ assets/vendor/datatables/index.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// @file /Users/davidannebicque/htdocs/intranetV3/assets/vendor/datatables/index.js
+// @author davidannebicque
+// @project intranetV3
+// @lastUpdate 05/07/2020 19:01
+
+import dt from './js/jquery.dataTables.min'
+import dtbs4 from './js/dataTables.bootstrap4.min'
+
+import dtcss from './css/dataTables.bootstrap4.min.css'
+
+
+
