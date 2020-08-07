5c051af8ebf14f5ff86a7e7452220e139cdefac1
David Annebicque <david.annebicque@gmail.com>
2020-08-07 12:23:38 +0200

    Refonte partie groupe et type de groupe

--- src/Controller/administration/TypeGroupeController.php
+++ src/Controller/administration/TypeGroupeController.php
@@ -3,14 +3,18 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/TypeGroupeController.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 07/08/2020 12:00
+// @lastUpdate 07/08/2020 12:23
 
 namespace App\Controller\administration;
 
+use App\Classes\MyGroupes;
 use App\Controller\BaseController;
 use App\Entity\Constantes;
+use App\Entity\Groupe;
 use App\Entity\Semestre;
 use App\Entity\TypeGroupe;
+use App\Repository\GroupeRepository;
+use App\Repository\TypeGroupeRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -56,6 +60,47 @@ class TypeGroupeController extends BaseController
 
         return new JsonResponse(true, Response::HTTP_OK);
 
+    }
+
+    /**
+     * @Route("/ajax/update/{id}", name="administration_type_groupe_ajax_edit", methods={"POST"},
+     *                             options={"expose":true})
+     * @param Request    $request
+     *
+     * @param TypeGroupe $typeGroupe
+     *
+     * @return JsonResponse|null
+     */
+    public function update(Request $request, TypeGroupe $typeGroupe): ?JsonResponse
+    {
+        $value = $request->request->get('value');
+
+        $typeGroupe->setLibelle($value);
+        $this->entityManager->flush();
+
+        return new JsonResponse(true, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/ajax/update-type", name="administration_typegroupe_change_type", methods={"POST"},
+     *                             options={"expose":true})
+     * @param TypeGroupeRepository $typeGroupeRepository
+     * @param Request              $request
+     *
+     * @return JsonResponse|null
+     */
+    public function updateType(TypeGroupeRepository $typeGroupeRepository, Request $request): ?JsonResponse
+    {
+        $typegroupe = $typeGroupeRepository->find($request->request->get('typegroupe'));
+
+        if ($typegroupe !== null) {
+            $typegroupe->setType($request->request->get('type'));
+            $this->entityManager->flush();
+
+            return new JsonResponse(true, Response::HTTP_OK);
+        }
+
+        return new JsonResponse(false, Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 
     /**
