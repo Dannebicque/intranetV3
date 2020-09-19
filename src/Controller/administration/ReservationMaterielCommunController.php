fa50a4b9b1094cf6a6646e1ac025fc447a59ecad
David Annebicque <david.annebicque@gmail.com>
2020-09-19 09:30:18 +0200

    Réservation de matériel commun

---
+++ src/Controller/administration/ReservationMaterielCommunController.php
@@ -0,0 +1,91 @@
+<?php
+// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/ReservationMaterielCommunController.php
+// @author davidannebicque
+// @project intranetV3
+// @lastUpdate 19/09/2020 09:29
+
+namespace App\Controller\administration;
+
+use App\Classes\MyMaterielCommun;
+use App\Classes\MyMaterielCommunReservation;
+use App\Controller\BaseController;
+use App\Repository\MaterielCommunPretRepository;
+use App\Repository\MaterielCommunRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AbsenceController
+ * @package App\Controller\administration
+ * @Route("/administration/materiel-commun/reservation")
+ */
+class ReservationMaterielCommunController extends BaseController
+{
+
+    /**
+     * @param MaterielCommunRepository $materielCommunRepository
+     * @param MaterielCommunPretRepository $materielCommunPretRepository
+     * @param MyMaterielCommun $myMaterielCommun
+     *
+     * @return Response
+     * @Route("/", name="administration_reservation_materiel_commun", methods={"GET"})
+     */
+    public function reservation(
+        MaterielCommunRepository $materielCommunRepository,
+        MaterielCommunPretRepository $materielCommunPretRepository,
+        MyMaterielCommun $myMaterielCommun
+    ) {
+
+        $emprunts = $materielCommunPretRepository->findAll();
+        $jr = $myMaterielCommun->getJours();
+        $dispomateriel = $myMaterielCommun->obtainDispoMateriel($jr);
+
+
+        return $this->render('administration/reservation_materiel_commun/reservation.html.twig', [
+            'materiels' => $materielCommunRepository->findAll(),
+            'emprunts'  => $emprunts,
+            'dispo'     => $dispomateriel,
+            'jours'     => $jr,
+            'creneau'   => ['0800', '0930', '1100', '1400', '1530', '1700']
+        ]);
+    }
+
+    /**
+     * @param MyMaterielCommunReservation $myReservationMaterielCommun
+     * @param Request                     $request
+     *
+     * @return Response
+     * @Route("/ajax/add", name="administration_reservation_materiel_commun_add", methods={"POST"},
+     *                     options={"expose"=true})
+     */
+    public function addReservation(MyMaterielCommunReservation $myReservationMaterielCommun, Request $request): Response
+    {
+        $t = explode('_', $request->request->get('id'));
+        $rep = $myReservationMaterielCommun->addReservation($t[2], $t[0], $t[1], $this->getConnectedUser());
+
+        return new Response($rep, $rep ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR);
+
+    }
+
+    /**
+     * @param MyMaterielCommunReservation $myReservationMaterielCommun
+     * @param Request                     $request
+     *
+     * @return Response
+     * @Route("/ajax/suppr", name="administration_reservation_materiel_commun_suppr", methods={"DELETE"},
+     *                       options={"expose"=true})
+     */
+    public function supprReservation(
+        MyMaterielCommunReservation $myReservationMaterielCommun,
+        Request $request
+    ): Response {
+        $id = $request->request->get('id');
+        $rep = $myReservationMaterielCommun->supprReservation($id);
+
+        return new Response($rep, $rep ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR);
+
+    }
+
+}
