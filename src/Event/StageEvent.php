a60aa0340226d7d136f15b6aa115ca17dbff5501
David Annebicque <david.annebicque@gmail.com>
2020-09-25 17:19:21 +0200

    Stage etape impression

--- src/Event/StageEvent.php
+++ src/Event/StageEvent.php
@@ -3,7 +3,7 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Event/StageEvent.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 25/09/2020 17:10
+// @lastUpdate 25/09/2020 17:18
 
 namespace App\Event;
 
@@ -15,7 +15,7 @@ class StageEvent extends Event
     public const CHGT_ETAT_STAGE_AUTORISE = 'chgt.etat_stage_autorise';
     public const CHGT_ETAT_STAGE_DEPOSE = 'chgt.etat_stage_depose';
     public const CHGT_ETAT_STAGE_VALIDE = 'chgt.etat_stage_valide';
-    public const CHGT_ETAT_STAGE_CONVENTION_IMPRIME = 'chgt.etat_stage_imprime';
+    public const CHGT_ETAT_STAGE_CONVENTION_IMPRIME = 'chgt.etat_stage_convention_imprimee';
     public const CHGT_ETAT_STAGE_CONVENTION_ENVOYEE = 'chgt.etat_stage_convention_envoyee';
     public const CHGT_ETAT_CONVENTION_RECUE = 'chgt.etat_stage_convention_recue';
 
