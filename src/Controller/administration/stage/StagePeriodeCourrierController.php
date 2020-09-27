9a49f00b9551da21913e085a2729591fa6e3bd6a
David Annebicque <david.annebicque@gmail.com>
2020-09-27 15:09:22 +0200

    Gestion des courriers de stage

--- src/Controller/administration/stage/StagePeriodeCourrierController.php
+++ src/Controller/administration/stage/StagePeriodeCourrierController.php
@@ -3,10 +3,11 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/stage/StagePeriodeCourrierController.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 27/09/2020 14:30
+// @lastUpdate 27/09/2020 15:09
 
 namespace App\Controller\administration\stage;
 
+use App\Classes\DatabaseTwigLoader;
 use App\Classes\MyStageMailTemplate;
 use App\Controller\BaseController;
 use App\Entity\StageEtudiant;
@@ -22,6 +23,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Twig\Environment;
 
 /**
  * Class StagePeriodeCourrierController
@@ -30,32 +32,38 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
  */
 class StagePeriodeCourrierController extends BaseController
 {
+
+    private $donnees = [
+        'etudiant'            => [
+            'prenom' => 'Etudiant',
+            'nom'    => 'De Test',
+        ],
+        'entreprise'          => [
+            'raisonSociale' => 'Fictive Compagnie',
+        ],
+        'tuteur'              => [
+            'prenom' => 'Pierre',
+            'nom'    => 'Dupond',
+        ],
+        'tuteurUniversitaire' => [
+            'prenom' => 'John',
+            'nom'    => 'Doe',
+        ],
+        'stagePeriode'        => [
+            'libelle'      => 'Période de stage fictive',
+            'responsables' => [
+                0 =>
+                    ['displayPr' => 'Paul Pierre']
+            ]
+        ],
+        'dateDebutStageFr'    => '01/01/2020',
+        'dateFinStageFr'      => '31/12/2020'
+
+    ];
+
+
     /**
-     * @Route("/test-mail", name="test_mail_stage_twig")
-     *
-     * @param StageEtudiantRepository  $stageEtudiantRepository
-     * @param EventDispatcherInterface $eventDispatcher
-     *
-     * @return Response
-     */
-    public function testMail(
-        StageEtudiantRepository $stageEtudiantRepository,
-        EventDispatcherInterface $eventDispatcher
-    ) {
-        $stageEtudiant = $stageEtudiantRepository->find(8);
-        $eventNotif = StageEvent::CHGT_ETAT_STAGE_AUTORISE;
-        $event = new StageEvent($stageEtudiant);
-
-
-        $eventDispatcher->dispatch($event, $eventNotif);
-        echo 'ok event';
-
-        return $this->render('test-mail.html.twig');
-    }
-
-
-    /**
-     * @Route("/apercu/{mail}", name="administration_stage_periode_courrier_apercu_defaut")
+     * @Route("/apercu-defaut/{mail}", name="administration_stage_periode_courrier_apercu_defaut")
      *
      * @param $mail
      *
@@ -63,39 +71,44 @@ class StagePeriodeCourrierController extends BaseController
      */
     public function apercuDefaut($mail): Response
     {
-        $donnees = [
-            'etudiant'            => [
-                'prenom' => 'Etudiant',
-                'nom'    => 'De Test',
-            ],
-            'entreprise'          => [
-                'raisonSociale' => 'Fictive Compagnie',
-            ],
-            'tuteur'              => [
-                'prenom' => 'Pierre',
-                'nom'    => 'Dupond',
-            ],
-            'tuteurUniversitaire' => [
-                'prenom' => 'John',
-                'nom'    => 'Doe',
-            ],
-            'stagePeriode'        => [
-                'libelle'      => 'Période de stage fictive',
-                'responsables' => [
-                    0 =>
-                        ['displayPr' => 'Paul Pierre']
-                ]
-            ],
-            'dateDebutStageFr'    => '01/01/2020',
-            'dateFinStageFr'      => '31/12/2020'
-
-        ];
-
-
         return $this->render('administration/stage/stage_periode_courrier/apercuDefaut.html.twig', [
             'mail'          => $mail,
-            'stageEtudiant' => $donnees
+            'stageEtudiant' => $this->donnees
         ]);
+    }
+
+    /**
+     * @Route("/apercu/{stagePeriode}/{mail}", name="administration_stage_periode_courrier_apercu_modele")
+     *
+     * @param $mail
+     *
+     * @return Response
+     */
+    public function apercu(
+        DatabaseTwigLoader $databaseTwigLoader,
+        StageMailTemplateRepository $stageMailTemplateRepository,
+        StagePeriode $stagePeriode,
+        $mail
+    ): Response {
+        $mailTemplate = $stageMailTemplateRepository->findEventPeriode(
+            $mail,
+            $stagePeriode
+        );
+
+        if ($mailTemplate !== null && $mailTemplate->getTwigTemplate() !== null) {
+            $twig = new Environment($databaseTwigLoader);
+            $mail = $twig->render($mailTemplate->getTwigTemplate()->getName(), ['stageEtudiant' => $this->donnees]);
+
+            return $this->render('administration/stage/stage_periode_courrier/apercu.html.twig', [
+                'mail' => $mail
+            ]);
+        }
+
+        return $this->render('administration/stage/stage_periode_courrier/apercu.html.twig', [
+            'mail' => null
+        ]);
+
+
     }
 
     /**
