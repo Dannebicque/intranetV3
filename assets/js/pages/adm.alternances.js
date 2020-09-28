0d1368e043878fbc5961c7b48483257f5bbced7e
David Annebicque <david.annebicque@gmail.com>
2020-09-28 17:53:11 +0200

    Alternance

--- assets/js/pages/adm.alternances.js
+++ assets/js/pages/adm.alternances.js
@@ -2,7 +2,7 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/assets/js/pages/adm.alternances.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 28/09/2020 17:46
+// @lastUpdate 28/09/2020 17:52
 import {addCallout} from '../util'
 import Swal from 'sweetalert2'
 
@@ -26,17 +26,19 @@ $(document).on('click', '.initAllAlternance', function (e) {
   e.preventDefault()
   const url = $(this).attr('href')
 
-  Swal({
+  Swal.fire({
     title: 'Etes vous sûr?',
     text: 'Vous allez initialiser tous les suivis d\'alternances. Cela effacera les données déjà présente. !',
-    type: 'warning',
+    icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
-    confirmButtonText: 'Oui, initialiser!',
-    cancelButtonText: 'Non, ne rien faire!',
-    confirmButtonClass: 'btn btn-primary',
-    cancelButtonClass: 'btn btn-secondary',
+    confirmButtonText: 'Oui, je confirme',
+    cancelButtonText: 'Non, Annuler',
+    customClass: {
+      confirmButton: 'btn btn-primary',
+      cancelButton: 'btn btn-secondary'
+    },
     buttonsStyling: false
   }).then(function (result) {
     if (result.value) {
@@ -45,14 +47,30 @@ $(document).on('click', '.initAllAlternance', function (e) {
         type: 'POST',
         success: function () {
           addCallout('Initialisation effectuée avec succès', 'success')
-          Swal(
-            'Initialisé!',
-            'Toutes les fiches des alertnants ont été initialisées.',
-            'success'
-          )
+          Swal.fire({
+            title: 'Alternances initialisées!',
+            text: 'Toutes les alternances ont été initialisées.',
+            icon: 'success',
+            confirmButtonText: 'OK',
+            customClass: {
+              confirmButton: 'btn btn-primary',
+              cancelButton: 'btn btn-secondary'
+            },
+            buttonsStyling: false
+          })
         },
         error: function () {
-          Swal('Error deleting!', 'Please try again', 'error')
+          Swal.fire({
+            title: 'Erreur lors de la tentative d\'initialisation!',
+            text: 'Merci de renouveler l\'opération',
+            icon: 'error',
+            confirmButtonText: 'OK',
+            customClass: {
+              confirmButton: 'btn btn-primary',
+              cancelButton: 'btn btn-secondary'
+            },
+            buttonsStyling: false
+          })
           addCallout('Erreur lors de la tentative d\'initialisation', 'danger')
         }
       })
@@ -61,11 +79,17 @@ $(document).on('click', '.initAllAlternance', function (e) {
       // Read more about handling dismissals
       result.dismiss === 'cancel'
     ) {
-      Swal(
-        'Cancelled',
-        'OK, on ne change rien...',
-        'error'
-      )
+      Swal.fire({
+        title: 'Cancelled',
+        text: 'OK! Tout est comme avant !',
+        icon: 'error',
+        confirmButtonText: 'OK',
+        customClass: {
+          confirmButton: 'btn btn-primary',
+          cancelButton: 'btn btn-secondary'
+        },
+        buttonsStyling: false
+      })
     }
   })
 })
