784f045bc61ae6c15ab224c95df60768e6c14305
David Annebicque <david.annebicque@gmail.com>
2020-09-29 11:46:44 +0200

    Testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// @file /Users/davidannebicque/htdocs/intranetV3/tests/bootstrap.php
+// @author davidannebicque
+// @project intranetV3
+// @lastUpdate 29/09/2020 11:46
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+    require dirname(__DIR__) . '/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+}
