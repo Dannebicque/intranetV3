4b8eebf80f402b2290e9dc50dbdb09026f018515
David Annebicque <david.annebicque@gmail.com>
2020-12-17 14:18:40 +0100

    Export toutes les notes

--- src/Controller/administration/NoteController.php
+++ src/Controller/administration/NoteController.php
@@ -3,10 +3,11 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/NoteController.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 12/12/2020 14:31
+// @lastUpdate 17/12/2020 14:06
 
 namespace App\Controller\administration;
 
+use App\Classes\Semestre\NotesExport;
 use App\Controller\BaseController;
 use App\Entity\Semestre;
 use App\Classes\MyEvaluations;
@@ -45,36 +46,18 @@ class NoteController extends BaseController
     /**
      * @Route("/all/semestre/{semestre}/export.{_format}", name="administration_all_notes_export", methods="GET",
      *                             requirements={"_format"="csv|xlsx|pdf"})
-     * @param MyExport       $myExport
-     * @param NoteRepository $noteRepository
+     * @param NotesExport    $notesExport
      * @param Semestre       $semestre
      * @param                $_format
      *
      * @return Response
      */
     public function exportAllNotes(
-        MyExport $myExport,
-        NoteRepository $noteRepository,
+        NotesExport $notesExport,
         Semestre $semestre,
         $_format
-    ): Response {
-        $notes = $noteRepository->findBySemestre($semestre, $semestre->getAnneeUniversitaire());
-
-        return $myExport->genereFichierGenerique(
-            $_format,
-            $notes,
-            'notes_' . $semestre->getLibelle(),
-            ['notes_administration', 'utilisateur', 'matiere'],
-            [
-                'date',
-                'heure',
-                'duree',
-                'etudiant'  => ['nom', 'prenom'],
-                'justifie',
-                'matiere'   => ['libelle'],
-                'personnel' => ['nom', 'prenom']
-            ]
-        );
+    ): ?Response {
+        return $notesExport->exportXlsToutesLesNotes($semestre, $this->dataUserSession->getAnneeUniversitaire());
     }
 
 
