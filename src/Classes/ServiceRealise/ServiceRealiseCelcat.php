59580db7c99b6b436f617698390a3d62c00f18ce
David Annebicque <david.annebicque@gmail.com>
2020-12-20 17:16:05 +0100

    Service par matière depuis Celcat

--- src/Classes/ServiceRealise/ServiceRealiseCelcat.php
+++ src/Classes/ServiceRealise/ServiceRealiseCelcat.php
@@ -3,7 +3,7 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/ServiceRealise/ServiceRealiseCelcat.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 19/12/2020 14:57
+// @lastUpdate 20/12/2020 16:46
 
 namespace App\Classes\ServiceRealise;
 
@@ -29,7 +29,14 @@ class ServiceRealiseCelcat implements ServiceRealiseInterface
 
     public function getServiceRealiseParMatiere(Matiere $matiere): array
     {
-        // TODO: Implement getServiceRealiseParMatiere() method.
+        $events = $this->celcatEventsRepository->findBy(['codeModule' => $matiere->getCodeElement()],
+            ['semaine' => 'ASC', 'jour' => 'ASC', 'debut' => 'ASC']);
+        $tabEvent = [];
+        foreach ($events as $event) {
+            $tabEvent[] = $this->convertToEvenementEdt($event);
+        }
+
+        return $tabEvent;
     }
 
     public function getServiceRealiserParEnseignant(Personnel $personnel): array
