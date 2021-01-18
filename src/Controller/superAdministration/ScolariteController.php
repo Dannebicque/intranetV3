6d05d29b5dce3d48a08476ab79a240f436153880
David Annebicque <david.annebicque@gmail.com>
2021-01-18 16:45:52 +0100

    Sous commission export Apogée

--- src/Controller/superAdministration/ScolariteController.php
+++ src/Controller/superAdministration/ScolariteController.php
@@ -1,15 +1,19 @@
 <?php
-// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/superAdministration/ScolariteController.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 05/07/2020 08:33
+// @lastUpdate 18/01/2021 16:45
 
 namespace App\Controller\superAdministration;
 
+use App\Classes\MyUpload;
+use App\Classes\SousCommission\SousCommissionExport;
+use App\Entity\Constantes;
 use App\Entity\Diplome;
 use App\Entity\Semestre;
 use App\Classes\Excel\MyExcelRead;
+use App\Repository\AnneeUniversitaireRepository;
 use App\Repository\DiplomeRepository;
 use App\Repository\SemestreRepository;
 use PhpOffice\PhpSpreadsheet\Exception;
@@ -48,7 +52,7 @@ class ScolariteController extends AbstractController
     public function diplomeShow(SemestreRepository $semestreRepository, Diplome $diplome): Response
     {
         return $this->render('super-administration/scolarite/diplome.html.twig', [
-            'diplome' => $diplome,
+            'diplome'   => $diplome,
             'semestres' => $semestreRepository->findByDiplome($diplome)
         ]);
     }
@@ -75,80 +79,34 @@ class ScolariteController extends AbstractController
     public function exportApogee(Semestre $semestre): Response
     {
         return $this->render('super-administration/scolarite/export-apogee.html.twig', [
-
+            'semestre' => $semestre
         ]);
     }
 
     /**
-     * @param Request     $request
-     * @param Semestre    $semestre
-     * @param MyExcelRead $myExcelRead
-     *
-     * @throws Exception
-     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
-     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     * @param AnneeUniversitaireRepository $anneeUniversitaireRepository
+     * @param SousCommissionExport         $sousCommissionExport
+     * @param Request                      $request
+     * @param Semestre                     $semestre
      * @Route("/genere-apogee/{semestre}",
      *     name="sa_scolarite_semestre_genere_apogee",
      *     methods={"POST"},
      *     requirements={"semestre":"\d+"})
      */
     public function genereFichierApogee(
+        AnneeUniversitaireRepository $anneeUniversitaireRepository,
+        SousCommissionExport $sousCommissionExport,
         Request $request,
-        Semestre $semestre,
-        MyExcelRead $myExcelRead
-    ): void {
-        $file = $request->files->get('fichier');
-        $originalName = $file->getClientOriginalName();
-        $dir = $this->container->getParameter('kernel.root_dir') . '/../web/uploads/apogee';
-        $file->move($dir, $originalName);
+        Semestre $semestre
+    ) {
+        $anneeUniersitaire = $anneeUniversitaireRepository->findOneBy(['active' => true]);
+        $resultat = $sousCommissionExport->exportApogee($semestre, $request->files->get('fichier'), $anneeUniersitaire);
+        if ($resultat === Constantes::PAS_DE_SOUS_COMM) {
+            $this->addFlash(Constantes::FLASHBAG_ERROR, 'Pas de sous commission validée');
 
-        $fichier = $dir . '/' . $originalName;
-        $myExcelRead->readFile($fichier);
-
-        $ligne = 18;
-
-        /* récupère les étudiants */
-        $tEtudiant = [];
-        while ($myExcelRead->getCellColLigne(1, $ligne) != '') {
-            $tEtudiant[$ligne] = trim($myExcelRead->getCellColLigne(0, $ligne));
-            $ligne++;
+            return $this->redirectToRoute('sa_scolarite_semestre_export_apogee', ['semestre' => $semestre->getId()]);
         }
 
-        /* récupère les modules */
-        $tModule = [];
-        $colonne = 4;
-        while ($myExcelRead->getCellColLigne($colonne, 14) != '') {
-            $val = explode('-', $myExcelRead->getCellColLigne($colonne, 14));
-            $tModule[$colonne] = trim($val[0]);
-            $colonne += 2; //3 si colonne résultat
-        }
-
-        $sousComm = $this->getDoctrine()->getRepository('DAKernelBundle:Scscommission')->findBy(['semestre' => $semestre->getId()]); //todo: si plusieurs ?
-        $scModule = $this->getDoctrine()->getRepository('DAKernelBundle:ScEtudiantModule')->findBy(['sc' => $sousComm[0]->getId()]);
-
-        $tEtu = [];
-        foreach ($scModule as $sc) {
-            $tEtu[$sc->getEtudiant()->getNumetudiant()][$sc->getMatiere()->getCodeapogee()] = $sc->getMoyenne();
-        }
-
-        foreach ($tEtudiant as $keyTe => $valueTe) {
-            foreach ($tModule as $keyTm => $valueTm) {
-                if (array_key_exists($valueTe, $tEtu) && array_key_exists($valueTm, $tEtu[$valueTe])) {
-                    $myExcelRead->writeCellColLigne($keyTm, $keyTe, number_format($tEtu[$valueTe][$valueTm], 2));
-                    $myExcelRead->writeCellColLigne($keyTm + 1, $keyTe, 20);
-                }
-            }
-
-        }
-
-        //EXPORT
-        unlink($fichier);
-        $myExcelRead->sauvegarde($dir . '/temp.xls');
-
-        $nom = explode('.', basename($fichier));
-        $this->transformeApogeeTexte($dir . '/temp.xls', $nom[0]);
-
-        unlink($dir . '/temp.xls');
-        exit();
+        return $resultat;
     }
 }
