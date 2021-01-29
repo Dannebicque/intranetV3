b583ef89554d5d97d4bd6bf871967ec6d1ab37f5
David Annebicque <david.annebicque@gmail.com>
2021-01-29 11:40:02 +0100

    Script Celcat

--- src/Controller/administration/EdtExportController.php
+++ src/Controller/administration/EdtExportController.php
@@ -3,13 +3,19 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/EdtExportController.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 27/01/2021 07:58
+// @lastUpdate 29/01/2021 11:39
 
 namespace App\Controller\administration;
 
 use App\Classes\Edt\MyEdtExport;
 use App\Controller\BaseController;
+use App\Entity\Constantes;
+use App\Entity\Groupe;
+use App\Entity\Semestre;
+use App\Repository\EdtPlanningRepository;
+use App\Repository\GroupeRepository;
 use App\Repository\PersonnelRepository;
+use App\Repository\SemestreRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -53,6 +59,57 @@ class EdtExportController extends BaseController
         return $this->render('administration/edtExport/index.html.twig', [
             'source' => $source
         ]);
+    }
+
+
+    /**
+     * @Route("/script/{source}", name="administration_edt_export_script", requirements={"source"="intranet|celcat"})
+     *
+     *
+     * @param SemestreRepository $semestreRepository
+     * @param GroupeRepository   $groupeRepository
+     * @param                    $source
+     *
+     * @return Response
+     */
+    public function exportScript(
+        SemestreRepository $semestreRepository,
+        GroupeRepository $groupeRepository,
+        $source
+    ): Response {
+        $semestres = $semestreRepository->findByDepartement($this->getDepartement());
+        $groupes = $groupeRepository->findByDepartement($this->getDepartement());
+
+        return $this->render('administration/edtExport/exportScript.html.twig', [
+            'semestres' => $semestres,
+            'groupes'   => $groupes
+        ]);
+    }
+
+    /**
+     * @Route("/script-ajax/{semestre}/{groupe}/{debut}/{fin}", name="administration_edt_export_script_ajax",
+     *                                                          options={"expose":true})
+     *
+     *
+     * @return Response
+     */
+    public function exportScriptAjax(
+        EdtPlanningRepository $edtPlanningRepository,
+        Semestre $semestre,
+        Groupe $groupe,
+        int $debut,
+        int $fin = 0
+    ): Response {
+        $pl = $edtPlanningRepository->findEdtSemestre($semestre, $debut);
+        $code = '';
+        foreach ($pl as $p) {
+            if ($p->getType() === $groupe->getTypeGroupe()->getType() && $p->getGroupe() === $groupe->getOrdre()) {
+                $code .= 'ajouter ' . $p->getJour() . ' ' . Constantes::TAB_HEURES[$p->getDebut()] . ' ' . Constantes::TAB_HEURES[$p->getFin()] . "\n";
+            }
+        }
+
+
+        return $this->json(['code' => $code]);
     }
 
     /**
