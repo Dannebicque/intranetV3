37b135c199bf0b88abbf26ae46be1133ebba6871
David Annebicque <david.annebicque@gmail.com>
2021-02-06 22:36:58 +0100

    Améliorations pages superadmin

--- src/Repository/DiplomeRepository.php
+++ src/Repository/DiplomeRepository.php
@@ -1,22 +1,22 @@
 <?php
-// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/htdocs/intranetV3/src/Repository/DiplomeRepository.php
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 05/07/2020 08:09
+// @lastUpdate 06/02/2021 22:27
 
 namespace App\Repository;
 
 use App\Entity\Departement;
 use App\Entity\Diplome;
+use App\Entity\TypeDiplome;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Diplome|null find($id, $lockMode = null, $lockVersion = null)
  * @method Diplome|null findOneBy(array $criteria, array $orderBy = null)
- * @method Diplome[]    findAll()
  * @method Diplome[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class DiplomeRepository extends ServiceEntityRepository
@@ -29,6 +29,16 @@ class DiplomeRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Diplome::class);
+    }
+
+    public function findAll()
+    {
+        return $this->createQueryBuilder('d')
+            ->join(TypeDiplome::class, 't', 'WITH', 'd.typeDiplome = t.id')
+            ->orderBy('t.libelle', 'ASC')
+            ->addOrderBy('d.libelle', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
     /**
@@ -59,9 +69,11 @@ class DiplomeRepository extends ServiceEntityRepository
     public function findActifs()
     {
         return $this->createQueryBuilder('d')
+            ->join(TypeDiplome::class, 't', 'WITH', 'd.typeDiplome = t.id')
             ->where('d.actif = :actif')
             ->setParameter('actif', true)
-            ->orderBy('d.libelle')
+            ->orderBy('t.libelle', 'ASC')
+            ->addOrderBy('d.libelle', 'ASC')
             ->getQuery()
             ->getResult();
     }
