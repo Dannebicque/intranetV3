9ac04c77093c4db337dc6f1065012558fb560fdc
dannebicque <david.annebicque@gmail.com>
2021-03-29 17:29:39 +0200

    Fix. Limite nb caractères commentaire note

--- assets/js/pages/notes.js
+++ assets/js/pages/notes.js
@@ -1,45 +1,55 @@
-// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/htdocs/intranetV3/assets/js/pages/notes.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 20/10/2020 18:42
+// @lastUpdate 29/03/2021 17:07
 
 import {addCallout} from '../util'
 
 $('.savegroupe').click(function () {
   const groupe = $(this).attr('id')
   const notes = {'notes': []}
+  let erreur = false
   $('.noteetudiant:input').each(function () {
     if ($(this).hasClass(groupe)) //vérifier que c'est le groupe concerné
     {
       const $id = $(this).data('etudiant')
+      const blCommentaire = $('#com_' + $id)
+      if (blCommentaire.val().length > 255) {
+        erreur = true
+        blCommentaire.addClass('is-invalid')
+      }
+
       const obj = {
         'id': $id,
         'note': $(this).val(),
         'absence': $('#abs_' + $id).prop('checked'),
-        'commentaire': $('#com_' + $id).val()
+        'commentaire': blCommentaire.val()
       }
 
       notes['notes'].push(obj)
     }
   })
-
-  $.ajax(
-    {
-      url: Routing.generate('application_personnel_note_ajax_saisie', {uuid: $(this).data('evaluation')}),
-      type: 'POST',
-      data: {
-        notes: notes
-      },
-      success: function (data) {
-        addCallout('Les notes de <strong>ce groupe</strong> ont été enregistrées avec succés ! Vous pouvez les modifier !', 'success')
-        //feature: supprimer le "orange" sur les notes sauvegardées. Peut être récupérer en data les notes.
-      },
-      error: function () {
-        addCallout('Une erreur est survenue pendant l\'envoi... <br>Veuillez réessayer', 'danger')
-        //feature: être plus précis ?
-      }
-    })
+  if (erreur === false) {
+    $.ajax(
+      {
+        url: Routing.generate('application_personnel_note_ajax_saisie', {uuid: $(this).data('evaluation')}),
+        type: 'POST',
+        data: {
+          notes: notes
+        },
+        success: function (data) {
+          addCallout('Les notes de <strong>ce groupe</strong> ont été enregistrées avec succés ! Vous pouvez les modifier !', 'success')
+          //feature: supprimer le "orange" sur les notes sauvegardées. Peut être récupérer en data les notes.
+        },
+        error: function () {
+          addCallout('Une erreur est survenue pendant l\'envoi... <br>Veuillez réessayer', 'danger')
+          //feature: être plus précis ?
+        }
+      })
+  } else {
+    addCallout('Le formulaire contient des erreurs. Veuillez corriger et valider de nouveau', 'danger')
+  }
 
 })
 
