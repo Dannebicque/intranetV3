8c3f732ef4be753675753b1b5ab4e2e63f95e081
David Annebicque <david.annebicque@gmail.com>
2021-05-13 17:05:31 +0200

    Feat. Import référentiel formation

--- src/Controller/superAdministration/ImportDiplomeController.php
+++ src/Controller/superAdministration/ImportDiplomeController.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/superAdministration/ImportDiplomeController.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 13/05/2021 17:04
+ * @lastUpdate 13/05/2021 17:05
  */
 
 namespace App\Controller\superAdministration;
@@ -15,6 +15,7 @@ use App\Controller\BaseController;
 use App\Entity\Constantes;
 use App\Entity\Diplome;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -24,14 +25,14 @@ class ImportDiplomeController extends BaseController
 {
     /**
      * @Route("/{id}", name="sa_diplome_import_index")
+     * @throws \Exception
      */
     public function index(
         MyUpload $myUpload,
         DiplomeImport $diplomeImport,
         Request $request,
         Diplome $diplome
-    )
-    {
+    ): Response {
         if ($request->isMethod('POST')) {
             $fichier = $myUpload->upload($request->files->get('fichier'), 'temp/', ['xml']);
             $diplomeImport->import($diplome, $fichier, $request->request->get('typeFichier'));
