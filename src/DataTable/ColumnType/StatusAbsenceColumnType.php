65025de781aef1271ad658e622d6f529a7b6d9b9
David Annebicque <david.annebicque@gmail.com>
2021-05-26 09:02:21 +0200

    Fix. Rattrapage DataTable

--- src/DataTable/ColumnType/StatusAbsenceColumnType.php
+++ src/DataTable/ColumnType/StatusAbsenceColumnType.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/DataTable/ColumnType/StatusAbsenceColumnType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 23/05/2021 14:21
+ * @lastUpdate 26/05/2021 09:01
  */
 
 namespace App\DataTable\ColumnType;
@@ -18,15 +18,17 @@ class StatusAbsenceColumnType extends PropertyColumnType
     {
         $absences = $options['absences'];
         $data = explode('_', $value);
-        if (count($absences) > 0) {
-            if (array_key_exists($data[0], $absences)) {
-                if (array_key_exists($data[1], $absences[$data[0]])) {
-                    if (array_key_exists($data[2], $absences[$data[0]][$data[1]])) {
-                        if (true === $absences[$data[0]][$data[1]][$data[2]]) {
-                            return '<span class="badge badge-success">Absence justifiée</span>';
-                        }
+        if (null !== $value) {
+            if (count($absences) > 0) {
+                if (array_key_exists($data[0], $absences)) {
+                    if (array_key_exists($data[1], $absences[$data[0]])) {
+                        if (array_key_exists($data[2], $absences[$data[0]][$data[1]])) {
+                            if (true === $absences[$data[0]][$data[1]][$data[2]]) {
+                                return '<span class="badge badge-success">Absence justifiée</span>';
+                            }
 
-                        return '<span class="badge badge-danger">Absence non justifiée</span>';
+                            return '<span class="badge badge-danger">Absence non justifiée</span>';
+                        }
                     }
                 }
             }
