afd2132374554c39aad8d732b5f2806bdfb69f4e
David Annebicque <david.annebicque@gmail.com>
2021-05-31 22:26:14 +0200

    Fix. Export Maquette

--- src/Classes/Apogee/GenereCodeApogee.php
+++ src/Classes/Apogee/GenereCodeApogee.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/Apogee/GenereCodeApogee.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 26/05/2021 18:22
+ * @lastUpdate 31/05/2021 22:26
  */
 
 namespace App\Classes\Apogee;
@@ -40,17 +40,17 @@ class GenereCodeApogee
     {
         $debut = 'T' . strtoupper($lettredomaine . $lettrediplome . 'X');
         //Diplôme
-        $this->diplome->setCodeDiplome('5' . $debut);
-        $this->diplome->setCodeEtape('');
-        $this->diplome->setCodeVersion('111');
-        $this->diplome->setCodeDepartement('985');
+//        $this->diplome->setCodeDiplome('5'.$debut);
+//        $this->diplome->setCodeEtape('');
+//        $this->diplome->setCodeVersion('111');
+//        $this->diplome->setCodeDepartement('985');
 
         //annees
         $annees = $this->diplome->getAnnees();
         foreach ($annees as $annee) {
-            $annee->setCodeEtape('5' . $debut . $annee->getOrdre());
-            $annee->setCodeVersion('111');
-            $annee->setCodeDepartement('985');
+//            $annee->setCodeEtape('5'.$debut.$annee->getOrdre());
+//            $annee->setCodeVersion('111');
+//            $annee->setCodeDepartement('985');
 
             $semestres = $annee->getSemestres();
             foreach ($semestres as $semestre) {
@@ -63,14 +63,26 @@ class GenereCodeApogee
 
                 $ressources = $this->entityManager->getRepository(ApcRessource::class)->findBySemestre($semestre);
                 foreach ($ressources as $ressource) {
-                    $ressource->setCodeElement($debut . $semestre->getOrdreLmd() . '-' . substr($ressource->getCodeMatiere(),
-                            -2));
+                    if (1 === count($ressource->getApcRessourceCompetences())) {
+                        $ue = $ressource->getApcRessourceCompetences()[0]->getCompetence()->getCouleur();
+                        $ressource->setCodeElement($debut . $semestre->getOrdreLmd() . $ue[1] . substr($ressource->getCodeMatiere(),
+                                -2));
+                    } else {
+                        $ressource->setCodeElement($debut . $semestre->getOrdreLmd() . 'M' . substr($ressource->getCodeMatiere(),
+                                -2));
+                    }
                 }
 
                 $saes = $this->entityManager->getRepository(ApcSae::class)->findBySemestre($semestre);
                 foreach ($saes as $sae) {
-                    $sae->setCodeElement($debut . $semestre->getOrdreLmd() . '-' . ((int)substr($sae->getCodeMatiere(),
-                                -2) + 50));
+                    if (1 === count($ressource->getApcRessourceCompetences())) {
+                        $ue = $sae->getApcSaeCompetences()[0]->getCompetence()->getCouleur();
+                        $sae->setCodeElement($debut . $semestre->getOrdreLmd() . $ue[1] . ((int)substr($sae->getCodeMatiere(),
+                                    -2) + 50));
+                    } else {
+                        $sae->setCodeElement($debut . $semestre->getOrdreLmd() . 'M' . ((int)substr($sae->getCodeMatiere(),
+                                    -2) + 50));
+                    }
                 }
             }
         }
