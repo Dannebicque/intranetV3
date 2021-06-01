7f2c6a273d4663d9faeb218d3919baad2f7a1682
David Annebicque <david.annebicque@gmail.com>
2021-06-01 16:24:16 +0200

    Fix. Structure diplome/annee

--- src/Form/ApcParcoursType.php
+++ src/Form/ApcParcoursType.php
@@ -4,13 +4,14 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/ApcParcoursType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 07/02/2021 10:59
+ * @lastUpdate 01/06/2021 16:24
  */
 
 namespace App\Form;
 
 use App\Entity\ApcParcours;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,8 +20,11 @@ class ApcParcoursType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle')
-            ->add('code');
+            ->add('libelle', TextType::class, ['label' => 'libelle'])
+            ->add('code', TextType::class, ['label' => 'code'])
+            ->add('codeDiplome', TextType::class, ['label' => 'codeDiplome', 'required' => false])
+            ->add('codeVersion', TextType::class, ['label' => 'codeVersion', 'required' => false])
+            ->add('codeDepartement', TextType::class, ['label' => 'codeDepartement', 'required' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/TypeDiplomeType.php
+++ src/Form/TypeDiplomeType.php
@@ -4,12 +4,13 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/TypeDiplomeType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 22/05/2021 18:37
+ * @lastUpdate 01/06/2021 11:58
  */
 
 namespace App\Form;
 
 use App\Entity\TypeDiplome;
+use App\Form\Type\YesNoType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,7 +29,8 @@ class TypeDiplomeType extends AbstractType
             ->add('sigle', TextType::class, ['label' => 'sigle'])
             ->add('nb_semestres', TextType::class, ['label' => 'nb_semestres'])
             ->add('niveau_entree', TextType::class, ['label' => 'niveau_entree'])
-            ->add('niveau_sortie', TextType::class, ['label' => 'niveau_sortie']);
+            ->add('niveau_sortie', TextType::class, ['label' => 'niveau_sortie'])
+            ->add('apc', YesNoType::class, ['label' => 'approche_competence']);
     }
 
     /**
