0f634b8fc5738b99a19d0d3c99385ac18c4c18dd
David Annebicque <david.annebicque@gmail.com>
2021-07-25 11:58:20 +0200

    fix: Mise à jour version SF + Notifier

--- config/preload.php
+++ config/preload.php
@@ -1,9 +1,11 @@
 <?php
-// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
-// @file /Users/davidannebicque/htdocs/intranetV3/config/preload.php
-// @author davidannebicque
-// @project intranetV3
-// @lastUpdate 04/12/2020 13:44
+/*
+ * Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/htdocs/intranetV3/config/preload.php
+ * @author davidannebicque
+ * @project intranetV3
+ * @lastUpdate 25/07/2021 11:46
+ */
 
 if (file_exists(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
