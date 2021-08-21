b625336a440d6ee8390702cdc84195893da75f93
David Annebicque <david.annebicque@gmail.com>
2021-08-21 12:40:55 +0200

    fix: Suppression du datepicker d'Umbrella.

--- src/Form/Type/CarbonDateTimePickerType.php
+++ src/Form/Type/CarbonDateTimePickerType.php
@@ -4,21 +4,41 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/Type/CarbonDateTimePickerType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 06/06/2021 11:42
+ * @lastUpdate 21/08/2021 12:20
  */
 
 namespace App\Form\Type;
 
 use App\Form\Transformer\CarbonToDateTimeTransformer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Umbrella\CoreBundle\Form\DatepickerType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 
 /**
  * Class YesNoType.
  */
 class CarbonDateTimePickerType extends AbstractType
 {
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        //$view->vars['attr']['is'] = 'date-picker';
+        $view->vars['attr']['autocomplete'] = 'off';
+
+        $jsOptions = [
+            'dateFormat' => $options['format'],
+            'enableTime' => $options['enable_time'],
+            'allowInput' => $options['allow_input'],
+//            'minDate' => $this->toDate($options['min'], $options['format']),
+//            'maxDate' => $this->toDate($options['max'], $options['format'])
+        ];
+
+        $view->vars['attr']['data-options'] = json_encode($jsOptions);
+
+        parent::buildView($view, $form, $options);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->addModelTransformer(new CarbonToDateTimeTransformer());
@@ -26,6 +46,6 @@ class CarbonDateTimePickerType extends AbstractType
 
     public function getParent(): ?string
     {
-        return DatepickerType::class;
+        return CarbonDateTimeType::class;
     }
 }
