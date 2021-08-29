6096dc5b8ab0b4ae2d92925e60d51c6f1f020a83
David Annebicque <david.annebicque@gmail.com>
2021-08-29 16:00:03 +0200

    fix: Type fichier Version.php

--- src/Classes/Version.php
+++ src/Classes/Version.php
@@ -4,11 +4,10 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/Version.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 21/07/2021 17:19
+ * @lastUpdate 29/08/2021 16:00
  */
 
 namespace App\Classes;
-
 
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -19,10 +18,9 @@ class Version
     public function __construct(KernelInterface $kernel)
     {
         $this->dir = $kernel->getProjectDir();
-
     }
 
-    public function version()
+    public function version(): string
     {
         $filename = $this->dir . '/package.json';
         $composerData = json_decode(file_get_contents($filename), true);
