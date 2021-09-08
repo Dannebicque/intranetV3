0af0e8abe387096a7ef10864662aabf5a284d432
David Annebicque <david.annebicque@gmail.com>
2021-09-08 16:55:30 +0200

    fix: Fix divers

--- src/Classes/LDAP/MyLdap.php
+++ src/Classes/LDAP/MyLdap.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/LDAP/MyLdap.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 08/09/2021 15:33
+ * @lastUpdate 08/09/2021 16:55
  */
 
 /*
@@ -14,8 +14,8 @@
 namespace App\Classes\LDAP;
 
 use Exception;
-use const LDAP_OPT_PROTOCOL_VERSION;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Ldap\Ldap;
 
 class MyLdap
 {
@@ -23,9 +23,6 @@ class MyLdap
 
     private ParameterBagInterface $parameterBag;
 
-    /**
-     * MyApogee constructor.
-     */
     public function __construct(ParameterBagInterface $parameterBag)
     {
         $this->parameterBag = $parameterBag;
@@ -34,11 +31,10 @@ class MyLdap
     public function connect(): void
     {
         try {
-            $this->ds = ldap_connect($this->parameterBag->get('LDAP_HOST'));
+            $this->ds = Ldap::create('ext_ldap', ['connection_string' => $this->parameterBag->get('LDAP_HOST')]);
            # ldap_set_option($this->ds, LDAP_OPT_PROTOCOL_VERSION, 3);
             if ($this->ds) {
-                ldap_bind($this->ds, $this->parameterBag->get('LDAP_LOGIN'),
-                    $this->parameterBag->get('LDAP_PASSWORD'));
+                $this->ds->bind($this->parameterBag->get('LDAP_LOGIN'), $this->parameterBag->get('LDAP_PASSWORD'));
             }
         } catch (Exception $e) {
         }
@@ -64,21 +60,25 @@ class MyLdap
     {
         $this->connect();
 
-        $sr = ldap_search($this->ds, $this->parameterBag->get('LDAP_BASE_DN'),
-            '(supannEmpId=' . $numeroHarpege . ')', ['uid', 'mail']);
+        $query = $this->ds->query($this->parameterBag->get('LDAP_BASE_DN'), '(supannEmpId=' . $numeroHarpege . ')');
+        $results = $query->execute();
 
-        $info = ldap_get_entries($this->ds, $sr);
-
-        echo $info['count'] . " entries returned\n";
-
-        if (1 === ldap_count_entries($this->ds, $sr)) {
-            $personnel = ldap_get_entries($this->ds, $sr);
-            $t['login'] = $personnel[0]['uid'][0];
-            $t['mail'] = $personnel[0]['mail'][0];
-            ldap_unbind($this->ds);
-
-            return $t;
+        foreach ($results as $entry) {
+            // Do something with the results
+            dump($entry);
         }
+
+//        $sr = ldap_search($this->ds, $this->parameterBag->get('LDAP_BASE_DN'),
+//            '(supannEmpId=' . $numeroHarpege . ')', ['uid', 'mail']);
+//
+//        if (1 === ldap_count_entries($this->ds, $sr)) {
+//            $personnel = ldap_get_entries($this->ds, $sr);
+//            $t['login'] = $personnel[0]['uid'][0];
+//            $t['mail'] = $personnel[0]['mail'][0];
+//            ldap_unbind($this->ds);
+//
+//            return $t;
+//        }
 
         return null;
     }
