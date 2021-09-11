7cf00d04e609297826dafa652fa99073a3503b01
David Annebicque <david.annebicque@gmail.com>
2021-09-11 12:45:00 +0200

    feat: Ajout d'une commande pour mette à jour les données étudiantes depuis Apogée

--- src/Classes/Apogee/ApogeeEtudiant.php
+++ src/Classes/Apogee/ApogeeEtudiant.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/Apogee/ApogeeEtudiant.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 11/09/2021 10:49
+ * @lastUpdate 11/09/2021 12:41
  */
 
 namespace App\Classes\Apogee;
@@ -41,7 +41,8 @@ class ApogeeEtudiant extends Apogee
             'etudiant' => [
                 'setNumEtudiant' => $data['COD_ETU'],
                 'setNumIne' => $data['COD_NNE_IND'] . $data['COD_CLE_NNE_IND'],
-                'setDateNaissance' => Tools::convertApogeeDateToObject($data['DATE_NAI_IND']), //en fr?
+                'setDateNaissance' => Tools::convertApogeeDateToObject($data['DATE_NAI_IND']),
+                'setLieuNaissance' => $data['LIB_VIL_NAI_ETU'],
                 'setPromotion' => $data['DAA_ETB'],
                 'setNom' => $data['LIB_NOM_PAT_IND'],
                 'setAnneeBac' => $data['DAA_OBT_BAC_IBA'],
@@ -68,7 +69,7 @@ class ApogeeEtudiant extends Apogee
     {
         $this->connect();
         $stid = $this->conn->prepare(
-            "SELECT INDIVIDU.COD_ETU, INDIVIDU.COD_NNE_IND, INDIVIDU.COD_CLE_NNE_IND, INDIVIDU.DATE_NAI_IND, DAA_ENT_ETB, LIB_NOM_PAT_IND, LIB_PR1_IND, NUM_TEL, COD_SEX_ETU, LIB_AD1, LIB_AD2, LIB_AD3, COD_BDI, COMMUNE.LIB_COM, LIB_PAY, DAT_MOD_IND, DAA_ETB, IND_BAC.DAA_OBT_BAC_IBA, COD_BAC FROM INS_ADM_ETP INNER JOIN INDIVIDU ON INDIVIDU.COD_IND = INS_ADM_ETP.COD_IND INNER JOIN ADRESSE ON ADRESSE.COD_IND = INS_ADM_ETP.COD_IND LEFT JOIN COMMUNE ON COMMUNE.COD_COM=ADRESSE.COD_COM LEFT JOIN PAYS ON PAYS.COD_PAY=ADRESSE.COD_PAY INNER JOIN IND_BAC ON INDIVIDU.COD_IND=IND_BAC.COD_IND WHERE COD_ETP=:codeetape AND COD_VRS_VET=:version AND (INDIVIDU.COD_ANU_SRT_IND IS NULL OR INDIVIDU.COD_ANU_SRT_IND='')");
+            "SELECT INDIVIDU.COD_ETU, INDIVIDU.COD_NNE_IND, INDIVIDU.COD_CLE_NNE_IND, INDIVIDU.DATE_NAI_IND, DAA_ENT_ETB, LIB_NOM_PAT_IND, LIB_PR1_IND, NUM_TEL, COD_SEX_ETU, LIB_AD1, LIB_AD2, LIB_AD3, COD_BDI, COMMUNE.LIB_COM, LIB_PAY, DAT_MOD_IND, DAA_ETB, IND_BAC.DAA_OBT_BAC_IBA, COD_BAC, INDIVIDU.LIB_VIL_NAI_ETU FROM INS_ADM_ETP INNER JOIN INDIVIDU ON INDIVIDU.COD_IND = INS_ADM_ETP.COD_IND INNER JOIN ADRESSE ON ADRESSE.COD_IND = INS_ADM_ETP.COD_IND LEFT JOIN COMMUNE ON COMMUNE.COD_COM=ADRESSE.COD_COM LEFT JOIN PAYS ON PAYS.COD_PAY=ADRESSE.COD_PAY INNER JOIN IND_BAC ON INDIVIDU.COD_IND=IND_BAC.COD_IND WHERE COD_ETP=:codeetape AND COD_VRS_VET=:version AND (INDIVIDU.COD_ANU_SRT_IND IS NULL OR INDIVIDU.COD_ANU_SRT_IND='')");
         $stid->execute([':codeetape' => $annee->getCodeEtape(), ':version' => $annee->getCodeVersion()]);
 
         return $stid;
