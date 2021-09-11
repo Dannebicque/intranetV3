c6e365ed6ab9651c67d2889c5081540d7a97918d
David Annebicque <david.annebicque@gmail.com>
2021-09-11 20:16:36 +0200

    fix: Tom-Select + utilisation sur des formulaires

--- assets/components/SelectComplete.js
+++ assets/components/SelectComplete.js
@@ -2,14 +2,18 @@
 // @file /Users/davidannebicque/htdocs/intranetV3/assets/components/SelectComplete.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 21/08/2021 11:50
+// @lastUpdate 11/09/2021 19:18
 
 import TomSelect from 'tom-select'
 
 export default class SelectComplete extends HTMLSelectElement {
 
   constructor () {
+    let options = {}
     super()
-    new TomSelect(this, {})
+    if (this.dataset.options) {
+      options = JSON.parse(this.dataset.options)
+    }
+    new TomSelect(this, options)
   }
 }
