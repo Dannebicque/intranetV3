142befa6cbd53c1878cb2e5c5b288f006b1e0b63
David Annebicque <david.annebicque@gmail.com>
2021-09-24 22:29:12 +0200

    feat: Amélioration du formulaire d'ajout d'un groupe

--- src/Form/GroupeType.php
+++ src/Form/GroupeType.php
@@ -4,13 +4,14 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/GroupeType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 23/05/2021 14:21
+ * @lastUpdate 24/09/2021 22:29
  */
 
 namespace App\Form;
 
 use App\Entity\Groupe;
 use App\Entity\Parcour;
+use App\Entity\Semestre;
 use App\Entity\TypeGroupe;
 use App\Repository\GroupeRepository;
 use App\Repository\ParcourRepository;
@@ -26,19 +27,24 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class GroupeType extends AbstractType
 {
-    private $semestre;
+    private ?Semestre $semestre;
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $this->semestre = $options['semestre'];
 
         $builder
-            ->add('libelle', TextType::class, ['label' => 'libelle'])
-            ->add('ordre', TextType::class, ['label' => 'ordre'])
-            ->add('codeApogee', TextType::class, ['label' => 'codeApogee'])
+            ->add('libelle', TextType::class, ['label' => 'label.libelle', 'help' => 'Libellé du groupe. Obligatoire.'])
+            ->add('ordre', TextType::class,
+                ['label' => 'label.ordre', 'help' => 'Ordre dans la liste des groupes. Obligatoire'])
+            ->add('codeApogee', TextType::class, [
+                'label' => 'label.codeApogee',
+                'help' => 'Nécessaire pour synchroniser les groupes depuis Apogée.',
+                'required' => false
+            ])
             ->add('typeGroupe', EntityType::class, [
                 'class' => TypeGroupe::class,
-                'label' => 'typeGroupe',
+                'label' => 'label.typeGroupe',
                 'choice_label' => 'libelle',
                 'query_builder' => function(TypeGroupeRepository $typeGroupeRepository) {
                     return $typeGroupeRepository->findBySemestreBuilder($this->semestre);
@@ -49,7 +55,7 @@ class GroupeType extends AbstractType
             ])
             ->add('parent', EntityType::class, [
                 'class' => Groupe::class,
-                'label' => 'groupe_parent',
+                'label' => 'label.groupe_parent',
                 'choice_label' => 'libelle',
                 'query_builder' => function(GroupeRepository $groupeRepository) {
                     return $groupeRepository->findBySemestreBuilder($this->semestre);
@@ -60,7 +66,7 @@ class GroupeType extends AbstractType
             ])
             ->add('parcours', EntityType::class, [
                 'class' => Parcour::class,
-                'label' => 'parcours',
+                'label' => 'label.parcours',
                 'choice_label' => 'libelle',
                 'query_builder' => function(ParcourRepository $parcourRepository) {
                     return $parcourRepository->findBySemestreBuilder($this->semestre);
@@ -76,6 +82,7 @@ class GroupeType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Groupe::class,
             'semestre'   => null,
+            'translation_domain' => 'form'
         ]);
     }
 }
