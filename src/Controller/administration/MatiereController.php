4227719e1abf51e720db62c6f23644f3e767bfdb
David Annebicque <david.annebicque@gmail.com>
2021-09-24 21:57:18 +0200

    fix: Suppression d'une matière

--- src/Controller/administration/MatiereController.php
+++ src/Controller/administration/MatiereController.php
@@ -4,43 +4,35 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/MatiereController.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 15/09/2021 20:33
+ * @lastUpdate 24/09/2021 21:57
  */
 
 namespace App\Controller\administration;
 
 use App\Classes\Configuration;
 use App\Classes\Exporter\ExporterManager;
-use App\Classes\Exporter\SourceIterator\DoctrineSourceIterator;
 use App\Classes\Exporter\SourceIterator\DtoSourceIterator;
+use App\Classes\Matieres\MatiereDelete;
 use App\Classes\Matieres\TypeMatiereManager;
-use App\Classes\MyExport;
 use App\Controller\BaseController;
-use App\DTO\MatiereCollection;
 use App\Entity\Constantes;
-use App\Entity\Date;
 use App\Entity\Diplome;
 use App\Entity\Matiere;
 use App\Entity\Ue;
 use App\Form\MatiereType;
 use App\Repository\ApcRessourceRepository;
 use App\Repository\ApcSaeRepository;
-use App\Repository\DateRepository;
 use App\Repository\MatiereRepository;
+use App\Utils\Tools;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use function count;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/administration/matiere")
- */
+#[Route('/administration/matiere')]
 class MatiereController extends BaseController
 {
-    /**
-     * @Route("/{diplome}", name="administration_matiere_index", methods="GET")
-     */
+    #[Route('/{diplome}', name: 'administration_matiere_index', methods: ['GET'])]
     public function index(Diplome $diplome = null): Response
     {
         return $this->render('administration/matiere/index.html.twig',
@@ -49,9 +41,7 @@ class MatiereController extends BaseController
             ]);
     }
 
-    /**
-     * @Route("/diplome/{diplome}", name="administration_matiere_diplome", methods="GET", options={"expose":true})
-     */
+    #[Route('/diplome/{diplome}', name: 'administration_matiere_diplome', options: ['expose' => true], methods: ['GET'])]
     public function diplome(
         MatiereRepository $matiereRepository,
         ApcRessourceRepository $apcRessourceRepository,
@@ -73,10 +63,11 @@ class MatiereController extends BaseController
         ]);
     }
 
-    /**
-     * @Route("/{diplome}/export.{_format}", name="administration_matiere_export", methods="GET",
-     *                             requirements={"_format"="csv|xlsx|pdf"}, options={"expose":true})
-     */
+    #[Route('/{diplome}/export.{_format}',
+        name: 'administration_matiere_export',
+        requirements: ['_format' => ['csv', 'xlsx', 'pdf']],
+        options: ['expose' => true],
+        methods: ['GET'])]
     public function export(
         ExporterManager $exporter,
         TypeMatiereManager $typeMatiereManager,
@@ -88,6 +79,7 @@ class MatiereController extends BaseController
 
         return $exporter->export($datas, $_format, 'matieres');
     }
+
 //    public function export(
 //        MyExport $myExport,
 //        TypeMatiereManager $typeMatiereManager,
@@ -117,12 +109,10 @@ class MatiereController extends BaseController
 //        );
 //    }
 
-    /**
-     * @Route("/new/{diplome}/{ue}", name="administration_matiere_new", methods="GET|POST")
-     */
+    #[Route('/new/{diplome}/{ue}', name: 'administration_matiere_new', methods: ['GET', 'POST'])]
     public function create(Configuration $configuration, Request $request, Diplome $diplome, Ue $ue = null): Response
     {
-        if ('1' === $configuration->get('MODIFICATION_PPN')) {
+        if (true === Tools::convertToBool($configuration->get('MODIFICATION_PPN'))) {
             $matiere = new Matiere();
             $form = $this->createForm(MatiereType::class, $matiere, [
                 'diplome' => $diplome,
@@ -152,17 +142,13 @@ class MatiereController extends BaseController
         return $this->redirectToRoute('erreur_666');
     }
 
-    /**
-     * @Route("/{id}/detail", name="administration_matiere_show", methods="GET")
-     */
+    #[Route('/{id}/detail', name: 'administration_matiere_show', methods: ['GET'])]
     public function show(Matiere $matiere): Response
     {
         return $this->render('administration/matiere/show.html.twig', ['matiere' => $matiere]);
     }
 
-    /**
-     * @Route("/{id}/ajax-edit", name="administration_matiere_ajax_edit", methods={"POST"}, options={"expose":true})
-     */
+    #[Route('/{id}/ajax-edit', name: 'administration_matiere_ajax_edit', options: ['expose' => true], methods: ['POST'])]
     public function ajaxEdit(
         Request $request,
         Matiere $matiere
@@ -177,12 +163,10 @@ class MatiereController extends BaseController
             Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 
-    /**
-     * @Route("/{id}/edit", name="administration_matiere_edit", methods="GET|POST")
-     */
+    #[Route('/{id}/edit', name: 'administration_matiere_edit', methods: ['GET', 'POST'])]
     public function edit(Configuration $configuration, Request $request, Matiere $matiere): Response
     {
-        if ('1' === $configuration->get('MODIFICATION_PPN')) {
+        if (true === Tools::convertToBool($configuration->get('MODIFICATION_PPN'))) {
             $form = $this->createForm(MatiereType::class, $matiere, [
                 'diplome' => $matiere->getSemestre()->getAnnee()->getDiplome(),
                 'attr' => [
@@ -207,12 +191,10 @@ class MatiereController extends BaseController
         return $this->redirectToRoute('erreur_666');
     }
 
-    /**
-     * @Route("/{id}/duplicate", name="administration_matiere_duplicate", methods="GET|POST")
-     */
+    #[Route('/{id}/duplicate', name: 'administration_matiere_duplicate', methods: ['GET', 'POST'])]
     public function duplicate(Configuration $configuration, Matiere $matiere): Response
     {
-        if ('1' === (int)$configuration->get('MODIFICATION_PPN')) {
+        if (true === Tools::convertToBool($configuration->get('MODIFICATION_PPN'))) {
             $newMatiere = clone $matiere;
 
             $this->entityManager->persist($newMatiere);
@@ -225,24 +207,22 @@ class MatiereController extends BaseController
         return $this->redirectToRoute('erreur_666');
     }
 
-    /**
-     * @Route("/{id}", name="administration_matiere_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Matiere $matiere)
-    {
+    #[Route('/{id}', name: 'administration_matiere_delete', methods: ['DELETE'])]
+    public function delete(
+        MatiereDelete $matiereDelete,
+        Request $request,
+        Matiere $matiere
+    ) {
         $id = $matiere->getId();
-        if ($this->isCsrfTokenValid('delete' . $id, $request->request->get('_token')) &&
-            0 === count($matiere->getEvaluations())) {
-            $this->entityManager->remove($matiere);
-            $this->entityManager->flush();
-            $this->addFlashBag(
-                Constantes::FLASHBAG_SUCCESS,
-                'matiere.delete.success.flash'
-            );
+        if ($this->isCsrfTokenValid('delete' . $id, $request->request->get('_token'))) {
+            $rep = $matiereDelete->delete($matiere);
 
-            return $this->json($id, Response::HTTP_OK);
+            if (true === $rep) {
+                $this->addFlashBag(Constantes::FLASHBAG_SUCCESS, 'matiere.success.error.flash');
+
+                return $this->json($id, Response::HTTP_OK);
+            }
         }
-
         $this->addFlashBag(Constantes::FLASHBAG_ERROR, 'matiere.delete.error.flash');
 
         return $this->json(false, Response::HTTP_INTERNAL_SERVER_ERROR);
