91598698757827b6527ae03fee83c0608bbde05b
David Annebicque <david.annebicque@gmail.com>
2021-10-25 11:54:56 +0200

    fix: Retour anciennes bornes

--- src/Classes/Edt/MyEdtBorne.php
+++ src/Classes/Edt/MyEdtBorne.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Classes/Edt/MyEdtBorne.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 23/10/2021 10:36
+ * @lastUpdate 25/10/2021 11:54
  */
 
 /*
@@ -14,8 +14,11 @@
 namespace App\Classes\Edt;
 
 use App\Classes\Matieres\TypeMatiereManager;
+use App\Entity\AnneeUniversitaire;
+use App\Entity\Semestre;
 use App\Exception\SemestreNotFoundException;
 use App\Repository\CalendrierRepository;
+use App\Repository\EdtPlanningRepository;
 use App\Repository\GroupeRepository;
 use App\Repository\SemestreRepository;
 
@@ -36,12 +39,14 @@ class MyEdtBorne
         CalendrierRepository $calendrierRepository,
         GroupeRepository $groupeRepository,
         EdtManager $edtManager,
-        SemestreRepository $semestreRepository
+        SemestreRepository $semestreRepository,
+        EdtPlanningRepository $edtPlanningRepository,
     ) {
         $this->calendrierRepository = $calendrierRepository;
         $this->groupeRepository = $groupeRepository;
         $this->edtManager = $edtManager;
         $this->semestreRepository = $semestreRepository;
+        $this->edtPlanningRepository = $edtPlanningRepository;
     }
 
     public function init(): void
@@ -55,6 +60,37 @@ class MyEdtBorne
     {
         return $this->data;
     }
+
+    /** @deprecated */
+    public function calculSemestre(
+        Semestre $semestre1,
+        Semestre $semestre2,
+        AnneeUniversitaire $anneeUniversitaire,
+        TypeMatiereManager $typeMatiereManager
+    ): void {
+        $semaine = $this->calendrierRepository->findOneBy([
+            'semaineReelle' => $this->data['semaine'],
+            'anneeUniversitaire' => $anneeUniversitaire->getId()
+        ]);
+
+        $this->data['semestre1'] = $semestre1;
+        $this->data['semestre2'] = $semestre2;
+        if (null !== $semaine) {
+
+            $this->data['p1']['planning'] = $this->edtPlanningRepository->recupereEDTBornes($semaine->getSemaineFormation(),
+                $semestre1, $this->data['jsem'], $typeMatiereManager->findBySemestreArray($semestre1));
+            $this->data['p2']['planning'] = $this->edtPlanningRepository->recupereEDTBornes($semaine->getSemaineFormation(),
+                $semestre2, $this->data['jsem'], $typeMatiereManager->findBySemestreArray($semestre2));
+
+
+            $this->data['p1']['groupes'] = $this->groupeRepository->findAllGroupes($semestre1);
+            $this->data['p2']['groupes'] = $this->groupeRepository->findAllGroupes($semestre2);
+            $this->data['jours'] = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+            $this->data['j1'] = $this->data['jours'][$this->data['jsem']] . ' ' . date('d/m/Y',
+                    mktime(12, 30, 00, date('n'), $this->data['njour'], date('Y')));
+        }
+    }
+
 
     public function getAffichageBorneJourSemestre(mixed $intSemestre, TypeMatiereManager $typeMatiereManager)
     {
