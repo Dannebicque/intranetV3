f8a75021ca3fc1f2e5c4479879f6da2501fe944c
David Annebicque <david.annebicque@gmail.com>
2021-10-25 16:42:49 +0200

    fix: Script ClickClick

--- src/Controller/administration/EdtExportController.php
+++ src/Controller/administration/EdtExportController.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Controller/administration/EdtExportController.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 26/08/2021 22:19
+ * @lastUpdate 25/10/2021 16:42
  */
 
 namespace App\Controller\administration;
@@ -14,11 +14,11 @@ use App\Classes\Matieres\TypeMatiereManager;
 use App\Controller\BaseController;
 use App\Entity\Constantes;
 use App\Entity\Personnel;
+use App\Repository\CalendrierRepository;
 use App\Repository\EdtPlanningRepository;
 use App\Repository\GroupeRepository;
 use App\Repository\PersonnelRepository;
 use App\Repository\SemestreRepository;
-use function array_key_exists;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -27,6 +27,7 @@ use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use ZipArchive;
+use function array_key_exists;
 
 /**
  * Class EdtController.
@@ -62,14 +63,17 @@ class EdtExportController extends BaseController
     public function exportScript(
         SemestreRepository $semestreRepository,
         GroupeRepository $groupeRepository,
+        CalendrierRepository $calendrierRepository,
         $source
     ): Response {
         $semestres = $semestreRepository->findByDepartementActif($this->getDepartement());
         $groupes = $groupeRepository->findByDepartementSemestreActif($this->getDepartement());
+        $semaines = $calendrierRepository->findByAnneeUniversitaire($this->getAnneeUniversitaire());
 
         return $this->render('administration/edtExport/exportScript.html.twig', [
             'semestres' => $semestres,
             'groupes' => $groupes,
+            'semaines' => $semaines
         ]);
     }
 
@@ -81,105 +85,78 @@ class EdtExportController extends BaseController
         KernelInterface $kernel,
         Request $request,
         GroupeRepository $groupeRepository,
+        CalendrierRepository $calendrierRepository,
         EdtPlanningRepository $edtPlanningRepository,
         SemestreRepository $semestreRepository
     ): Response {
         $semestre = $semestreRepository->find($request->request->get('semestre'));
         $semaine = $request->request->get('semaine');
+        $calendrier = $calendrierRepository->findOneBy([
+            'semaineFormation' => $semaine,
+            'anneeUniversitaire' => $this->getAnneeUniversitaire()->getId()
+        ]);
 
-        $tabProf = [
-            16041,
-            33407,
-            34188,
-            3601,
-            33736,
-            33513,
-            32822,
-            3132,
-            30722,
-            31405,
-            3604,
-            33740,
-            30990,
-            25261,
-            3680,
-            3994,
-            22885,
-            2569,
-            6509,
-            20289,
-            24594,
-            20900,
-            3920,
-            30125,
-            22615,
-            2158,
-            14478,
-            7679,
-            34044,
-            21399,
-            4954,
-        ];
-        $tabProf = array_flip($tabProf);
+//        $tabProf = [
+//            16041,
+//            33407,
+//            34188,
+//            3601,
+//            33736,
+//            33513,
+//            32822,
+//            3132,
+//            30722,
+//            31405,
+//            3604,
+//            33740,
+//            30990,
+//            25261,
+//            3680,
+//            3994,
+//            22885,
+//            2569,
+//            6509,
+//            20289,
+//            24594,
+//            20900,
+//            3920,
+//            30125,
+//            22615,
+//            2158,
+//            14478,
+//            7679,
+//            34044,
+//            21399,
+//            4954,
+//        ];
+//        $tabProf = array_flip($tabProf);
 
         $tabMatieres = [
             'S1' => [
-                '3TW11101' => 0,
-                '3TW11102' => 1,
-                '3TW11103' => 2,
-                '3TW11104' => 3,
-                '3TW11105' => 4,
-                '3TW11106' => 5,
-                '3TW11107' => 6,
-                '3TW11108' => 7,
-                '3TW11109' => 8,
-                '3TW11201' => 9,
-                '3TW11202' => 10,
-                '3TW11203' => 11,
-                '3TW11204' => 12,
-                '3TW11205' => 13,
-                '3TW11206' => 14,
-                '3TW11207' => 15,
-            ],
-            'S2' => [
-                '3TW22101' => 0,
-                '3TW22102' => 1,
-                '3TW22103' => 2,
-                '3TW22104' => 3,
-                '3TW22105' => 4,
-                '3TW22106' => 5,
-                '3TW22107' => 6,
-                '3TW22108' => 7,
-                '3TW22109' => 8,
-                '3TW22110' => 9,
-                '3TW22201' => 10,
-                '3TW22202' => 11,
-                '3TW22203' => 12,
-                '3TW22204' => 13,
-                '3TW22205' => 14,
-                '3TW22206' => 15,
-                '3TW22207' => 16,
-                '3TW22210' => 17,
-            ],
-            'S3D' => [
-                '3TW73101' => 0,
-                '3TW73102' => 1,
-                '3TW73103' => 2,
-                '3TW73104' => 3,
-                '3TW73105' => 4,
-                '3TW73106' => 5,
-                '3TW73107' => 6,
-                '3TW73108' => 7,
-                '3TW73109' => 8,
-                '3TW73110' => 9,
-                '3TW73201' => 10,
-                '3TW73202' => 11,
-                '3TW73203' => 12,
-                '3TW73204' => 13,
-                '3TW73205' => 14,
-                '3TW73206' => 15,
-                '3TW73207' => 16,
-                '3TW73212' => 17,
+                'TSBZ1M01' => 0,
+                'TSBZ1202' => 1,
+                'TSBZ1103' => 2,
+                'TSBZ1104' => 3,
+                'TSBZ1M05' => 4,
+                'TSBZ1M06' => 5,
+                'TSBZ1307' => 6,
+                'TSBZ1308' => 7,
+                'TSBZ1M09' => 8,
+                'TSBZ1310' => 9,
+                'TSBZ1412' => 10,
+                'TSBZ1413' => 11,
+                'TSBZ1415' => 12,
+                'TSBZ1M16' => 13,
+                'TSBZ1517' => 14,
+                'TSBZ1M18' => 15,
+                'TSBZ1519' => 16,
+                'TSBZ1151' => 17,
+                'TSBZ1252' => 18,
+                'TSBZ1353' => 19,
+                'TSBZ1354' => 20,
+                'TSBZ1455' => 21,
+                'TSBZ1556' => 22,
+                'TSBZ1M60' => 23,
             ],
             'S4' => [
                 '3TW4X101' => 0,
@@ -234,17 +211,8 @@ class EdtExportController extends BaseController
             'DIS5',
             'DIS6',
             'DIS7',
-            'DIS',
-            'H007+DIS1',
-            'H006+DIS2',
-            'H005+DIS3',
-            'DIS4+H201',
-            'DIS1+H007',
-            'DIS3+H205',
-            'H008+DIS2',
-            'H008+DIS1',
-            'H007+DIS2',
-            'DIS4+H005',
+            'DIS8',
+            'DISLP',
         ];
         $tabSalles = array_flip($tabSalles);
 
@@ -263,11 +231,18 @@ class EdtExportController extends BaseController
             }
         }
 
+
         foreach ($pl as $p) {
+            if (null !== $p->getIntervenant()) {
+                $codeprof = $p->getIntervenant()->getNumeroHarpege();
+            } else {
+                $codeprof = 0;
+            }
+
+
             if (
-                null !== $p->getIntervenant() &&
-                null !== $p->getMatiere() &&
-                array_key_exists($p->getIntervenant()->getNumeroHarpege(), $tabProf) &&
+                0 !== $p->getIdMatiere() &&
+                // array_key_exists($p->getIntervenant()->getNumeroHarpege(), $tabProf) &&
                 array_key_exists($p->getSalle(), $tabSalles)) {
                 /*
                  * # 1= jour de la semaine (ex: 1 pour lundi)
@@ -279,17 +254,18 @@ class EdtExportController extends BaseController
 # 7= type de cours (CM=1, TD=4, TP=6)
                  */
                 $codeMatiere = $matieres[$p->getTypeIdMatiere()]->codeElement;
-                $code[mb_strtoupper($p->getType())][$p->getGroupe()] .= './ajouter ' . $p->getJour() . ' ' . Constantes::TAB_HEURES[$p->getDebut()] . ' ' . Constantes::TAB_HEURES[$p->getFin()] . ' ' . $tabProf[$p->getIntervenant()->getNumeroHarpege()] . ' ' . $tabSalles[$p->getSalle()] . ' ' . $tabMatieres[$semestre->getLibelle()][$codeMatiere] . ' ' . $tabType[mb_strtoupper($p->getType())] . "\n";
+                $code[mb_strtoupper($p->getType())][$p->getGroupe()] .= './ajouter ' . $p->getJour() . ' ' . Constantes::TAB_HEURES[$p->getDebut()] . ' ' . Constantes::TAB_HEURES[$p->getFin()] . ' ' . $codeprof . ' ' . $tabSalles[$p->getSalle()] . ' ' . $tabMatieres[$semestre->getLibelle()][$codeMatiere] . ' ' . $tabType[mb_strtoupper($p->getType())] . "\n";
             }
-            if ('H018' === $p->getSalle()) {
-                $code[mb_strtoupper($p->getType())][$p->getGroupe()] .= './ajouterh018 ' . $p->getJour() . ' ' . Constantes::TAB_HEURES[$p->getDebut()] . ' ' . Constantes::TAB_HEURES[$p->getFin()] . ' ' . $tabProf[$p->getIntervenant()->getNumeroHarpege()] . ' 0 ' . $tabMatieres[$semestre->getLibelle()][$codeMatiere] . ' ' . $tabType[mb_strtoupper($p->getType())] . "\n";
+            if ('H018' === $p->getSalle()) // array_key_exists($p->getIntervenant()->getNumeroHarpege(), $tabProf))
+            {
+                $code[mb_strtoupper($p->getType())][$p->getGroupe()] .= './ajouterh018 ' . $p->getJour() . ' ' . Constantes::TAB_HEURES[$p->getDebut()] . ' ' . Constantes::TAB_HEURES[$p->getFin()] . ' ' . $codeprof . ' 0 ' . $tabMatieres[$semestre->getLibelle()][$codeMatiere] . ' ' . $tabType[mb_strtoupper($p->getType())] . "\n";
             }
         }
 
         $codeComplet = '';
 
         $zip = new ZipArchive();
-        $zipName = $kernel->getProjectDir() . '/public/upload/temp/ajouter_S' . $semaine . '_' . $semestre->getLibelle() . '.zip';
+        $zipName = $kernel->getProjectDir() . '/public/upload/temp/ajouter_S' . $calendrier->getSemaineReelle() . '_' . $semestre->getLibelle() . '.zip';
         @unlink($zipName);
         $zip->open($zipName, ZipArchive::CREATE);
         $i = 0;
@@ -297,7 +273,7 @@ class EdtExportController extends BaseController
             foreach ($value as $groupe => $c) {
                 $codeComplet .= './groupe ' . $i . "\n";
 
-                $n = $semestre->getLibelle() . '_S' . $semaine . '_' . $type . '_' . $codeGroupe[$type . '_' . $groupe] . '.sh';
+                $n = $semestre->getLibelle() . '_S' . $calendrier->getSemaineReelle() . '_' . $type . '_' . $codeGroupe[$type . '_' . $groupe] . '.sh';
 
                 $zip->addFromString($n, $c);
                 $codeComplet .= './' . $n . " \n";
@@ -306,13 +282,13 @@ class EdtExportController extends BaseController
             }
         }
 
-        $zip->addFromString('script' . $semaine . '.sh', $codeComplet);
+        $zip->addFromString('script' . $calendrier->getSemaineReelle() . '.sh', $codeComplet);
         $zip->close();
 
         $response = new Response(file_get_contents($zipName));
         $response->headers->set('Content-Type', 'application/zip');
         $response->headers->set('Content-Disposition',
-            'attachment;filename="ajouter_S' . $semaine . '_' . $semestre->getLibelle() . '.zip"');
+            'attachment;filename="ajouter_S' . $calendrier->getSemaineReelle() . '_' . $semestre->getLibelle() . '.zip"');
         $response->headers->set('Content-length', filesize($zipName));
 
         return $response;
