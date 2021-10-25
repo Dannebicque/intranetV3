bb8d21af68e2e009f85531857f187f15fd6744e3
David Annebicque <david.annebicque@gmail.com>
2021-10-25 18:00:52 +0200

    fix: Gestion des étudiants (semestre/département) en super admin

--- assets/js/pages/sadm.scolarite.js
+++ assets/js/pages/sadm.scolarite.js
@@ -1,16 +1,15 @@
-// Copyright (c) 2020. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/htdocs/intranetV3/assets/js/pages/sadm.scolarite.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 21/09/2020 15:49
+// @lastUpdate 25/10/2021 17:45
 import {addCallout} from '../util'
 
-$(document).on('change', '#chgt_etudiant_departement', function () {
+$(document).on('change', '.chgt_etudiant_departement', function () {
   $.ajax({
     url: Routing.generate('user_change_departement', {etudiant: $(this).data('etudiant'), departement: $(this).val()}),
     method: 'POST',
     success: function (e) {
-      $('#liste_groupes').empty().append('<tr><td colspan="3">Aucun groupe</td></tr>')
       addCallout('Mofification enregistrée !', 'success')
     },
     error: function (e) {
@@ -20,7 +19,21 @@ $(document).on('change', '#chgt_etudiant_departement', function () {
   })
 })
 
-$(document).on('change', '#chgt_etudiant_fin', function () {
+$(document).on('change', '.chgt_etudiant_semestre', function () {
+  $.ajax({
+    url: Routing.generate('user_change_semestre', {etudiant: $(this).data('etudiant'), semestre: $(this).val()}),
+    method: 'POST',
+    success: function (e) {
+      addCallout('Mofification enregistrée !', 'success')
+    },
+    error: function (e) {
+      addCallout('Erreur lors de la sauvegarde de la modification !', 'danger')
+    }
+
+  })
+})
+
+$(document).on('change', '.chgt_etudiant_fin', function () {
   $.ajax({
     url: Routing.generate('user_change_annee_sortie', {etudiant: $(this).data('etudiant'), annee: $(this).val()}),
     method: 'POST',
