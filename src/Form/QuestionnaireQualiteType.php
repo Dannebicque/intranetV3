ec62750b30188c17dc5e0d85c4049ee54c9d6b49
David Annebicque <david.annebicque@gmail.com>
2021-11-03 17:36:28 +0100

    feat: Gestion des sections et des réponses pour les questionnaires

--- src/Form/QuestionnaireQualiteType.php
+++ src/Form/QuestionnaireQualiteType.php
@@ -4,13 +4,20 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/QuestionnaireQualiteType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 09/10/2021 11:05
+ * @lastUpdate 03/11/2021 17:36
  */
 
 namespace App\Form;
 
 use App\Entity\QuestionnaireQualite;
+use App\Entity\Semestre;
+use App\Form\Type\CarbonDateTimeType;
+use App\Form\Type\CollectionStimulusType;
+use App\Form\Type\EntityCompleteType;
+use App\Repository\SemestreRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,23 +26,64 @@ class QuestionnaireQualiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle')
-            ->add('dateOuverture')
-            ->add('dateFermeture')
-            ->add('titre')
-            ->add('texteExplication')
-            ->add('texteDebut')
-            ->add('textFin')
-            ->add('uuid')
-            ->add('created')
-            ->add('updated')
-            ->add('semestre');
+            ->add('libelle', TextType::class, [
+                'label' => 'label.libelle',
+                'help' => 'Libellé court, affiché dans l\'administration',
+            ])
+            ->add('titre', TextType::class, [
+                'label' => 'label.titre',
+                'help' => 'Titre du questionnaire affiché aux étudiants',
+            ])
+            ->add('texteExplication', TextareaType::class, [
+                'label' => 'label.texteExplication',
+                'help' => 'Texte d\'explication du questionnaire. Affiché dans la liste des questionnaires.',
+            ])
+            ->add('texteDebut', TextareaType::class, [
+                'label' => 'label.texteDebut',
+                'help' => 'Texte introductif du questionnaire, affiché au début de l\'enquête',
+            ])
+            ->add('textFin', TextareaType::class, [
+                'label' => 'label.texteFin',
+                'help' => 'Texte affiché à la fin du questionnaire, une fois l\'ensemble des sections complétées.',
+            ])
+            ->add('semestre', EntityCompleteType::class, [
+                'label' => 'label.semestre',
+                'choice_label' => 'display',
+                'class' => Semestre::class,
+                'query_builder' => function(SemestreRepository $semestreRepository) {
+                    return $semestreRepository->findSemestresActifBuilder();
+                },
+                'help' => 'Semestre concerné par l\'enquête.',
+            ])
+            ->add('dateOuverture', CarbonDateTimeType::class, [
+                'label' => 'label.dateOuverture',
+                'required' => false,
+                'help' => 'Date d\'ouverture du questionnaire. Peut être complété par la suite.',
+            ])
+            ->add('dateFermeture', CarbonDateTimeType::class, [
+                'label' => 'label.dateFermeture',
+                'required' => false,
+                'help' => 'Date de fermeture du questionnaire. Peut être complété par la suite.',
+            ])
+            ->add('sections', CollectionStimulusType::class, [
+                'entry_type' => QuestionnaireQuestionnaireSectionType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'prototype' => true,
+                'allow_delete' => true,
+                'label' => 'Sections pour le questionnaire',
+                'by_reference' => false,
+                'max_items' => 0,
+
+                //'help' => 'Ajoutez les situations professionnelles de la compétence.',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => QuestionnaireQualite::class,
+            'translation_domain' => 'form',
         ]);
     }
 }
--- src/Form/QuestionnaireSectionType.php
+++ src/Form/QuestionnaireSectionType.php
@@ -4,33 +4,69 @@
  * @file /Users/davidannebicque/htdocs/intranetV3/src/Form/QuestionnaireSectionType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 09/10/2021 11:05
+ * @lastUpdate 03/11/2021 17:36
  */
 
 namespace App\Form;
 
 use App\Entity\QuestionnaireSection;
+use App\Form\Type\CollectionStimulusType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class QuestionnaireSectionType extends AbstractType
 {
+    private array $listeSection;
+    private array $listeSectionAdapter;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->listeSection = $options['listeSection'];
+        $this->listeSectionAdapter = $options['listeSectionAdapter'];
         $builder
-            ->add('titre')
-            ->add('textExplicatif')
-            ->add('config')
-            ->add('typeCalcul')
-            ->add('created')
-            ->add('updated');
+            ->add('titre', TextType::class, [
+                'label' => 'label.titre'
+            ])
+            ->add('textExplicatif', TextareaType::class, [
+                'label' => 'label.text_explicatif',
+                'help' => 'Texte introductif de la section, affiché en début de page',
+                'required' => false
+            ])
+            ->add('typeSection', ChoiceType::class, [
+                'choices' => $this->listeSection,
+                'label' => 'label.typeSection',
+                'expanded' => true,
+            ])
+            ->add('sectionAdapter', ChoiceType::class, [
+                'choices' => $this->listeSectionAdapter,
+                'label' => 'label.sectionAdapter',
+                'expanded' => true,
+                'mapped' => false
+            ])
+            ->add('qualiteSectionQuestions', CollectionStimulusType::class, [
+                'entry_type' => QuestionnaireSectionQuestionType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'prototype' => true,
+                'allow_delete' => true,
+                'label' => 'Questions pour la question',
+                'by_reference' => false,
+                'max_items' => 0,
+                'help' => 'Il est possible d\'ajouter les questions plus tard',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => QuestionnaireSection::class,
+            'listeSection' => null,
+            'listeSectionAdapter' => null
+
         ]);
     }
 }
