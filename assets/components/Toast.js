3d956876915580eeb8195ea2fa77fd1a5f498f0c
David Annebicque <david.annebicque@gmail.com>
2021-12-05 11:29:45 +0100

    feat: Suppression de Toastyfy, et utilisation du toast de BS.

--- assets/components/Toast.js
+++ assets/components/Toast.js
@@ -4,15 +4,13 @@
 // @project intranetV3
 // @lastUpdate 01/10/2021 13:06
 
-// import Toastify from 'toastify-js'
 import {Toast as ToastBs} from 'bootstrap'
 
 class Toast {
 
-  createAndShow (type, text, title = null, options = {})
-  {
+  createAndShow (type, text, title = null, options = {}) {
     let html = '<div class="position-fixed top-0 end-0 p-3" style="z-index:1000">\n' +
-      '<div class="toast align-items-center text-white bg-'+type+' border-0" role="alert" aria-live="assertive" aria-atomic="true">\n' +
+      '<div class="toast align-items-center text-white bg-' + type + ' border-0" role="alert" aria-live="assertive" aria-atomic="true">\n' +
       '  <div class="d-flex">\n' +
       '    <div class="toast-body">\n' +
       text + '\n'
@@ -21,26 +19,17 @@ class Toast {
     '  </div>\n' +
     '</div>\n' +
     '</div>'
-    console.log(document.body.lastChild)
 
-    var div=document.createElement("div");
-    div.innerHTML=html;
+    const div = document.createElement('div')
+    div.innerHTML = html
+    div.children[0].addEventListener('hidden.bs.toast', function (elem) {
+      elem.currentTarget.remove() //supprimer le code HTML après l'event.
+    })
     document.body.append(div.children[0])
 
     this.show(type, text, title, options)
   }
 
-  // defaultOptions = {
-  //   close: true,
-  //   duration: 3000,
-  //   className: 'toast',
-  //   //escapeMarkup: false,
-  //   gravity: 'top',
-  //   position: 'right',
-  //   stopOnFocus: true
-  // }
-
-  // todo: supprimer le code HTML après l'event.
 
   show (type, text, title = null, options = {}) {
     //options = {...this.defaultOptions, ...options}
@@ -55,20 +44,19 @@ class Toast {
   }
 
   error (text, title = null, options = {}) {
-    this.show('error', text, title, options)
+    this.createAndShow('error', text, title, options)
   }
 
   warning (text, title = null, options = {}) {
-    this.show('warning', text, title, options)
+    this.createAndShow('warning', text, title, options)
   }
 
   success (text, title = null, options = {}) {
-    console.log(text)
-    this.show('success', text, title, options)
+    this.createAndShow('success', text, title, options)
   }
 
   info (text, title = null, options = {}) {
-    this.show('info', text, title, options)
+    this.createAndShow('info', text, title, options)
   }
 
 }
