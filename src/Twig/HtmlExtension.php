1c3934d7c3e60827cfbfe558f66053d7ac38920f
David Annebicque <david.annebicque@gmail.com>
2022-01-19 17:36:14 +0100

    fix: [APC] Améliortion lisibilité des listes Ressources et SAE

--- src/Twig/HtmlExtension.php
+++ src/Twig/HtmlExtension.php
@@ -3,16 +3,22 @@
 namespace App\Twig;
 
 use ReflectionClass;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class HtmlExtension extends AbstractExtension
 {
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
     public function getFilters(): array
     {
         return [
             new TwigFilter('fqn', [$this, 'fqn']),
             new TwigFilter('deuxDigits', [$this, 'deuxDigits']),
+            new TwigFilter('badgeYesNo', [$this, 'badgeYesNo'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -26,5 +32,14 @@ class HtmlExtension extends AbstractExtension
     public function deuxDigits(string $value): string
     {
         return sprintf('%02d', $value);
+    }
+
+    public function badgeYesNo(bool $value): string
+    {
+        if (true === $value) {
+            return '<span class="badge bg-success"><i class="fas fa-check me-1"></i> '.$this->translator->trans('Yes').'</span>';
+        }
+
+        return '<span class="badge bg-danger"><i class="fas fa-ban me-1"></i> '.$this->translator->trans('No').'</span>';
     }
 }
