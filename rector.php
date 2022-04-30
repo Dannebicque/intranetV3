cd64c86d34af3dffb6ab4508a61b668e87507c4e
davidannebicque <david.annebicque@gmail.com>
2022-04-30 13:55:06 +0200

    feat: [rector] configuration corrigée pour PHP8.1

--- rector.php
+++ rector.php
@@ -2,26 +2,26 @@
 
 declare(strict_types=1);
 
+use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Core\Configuration\Option;
 use Rector\Symfony\Set\SymfonySetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // region Symfony Container
+return static function (RectorConfig $containerConfigurator): void {
+    // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(
         Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER,
         __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml'
     );
-    // endregion
+    // Define what rule sets will be applied
 
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_54);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    $containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+//    $containerConfigurator->import(SymfonySetList::SYMFONY_54);
+//    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+//    $containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+//    $containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
+//    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
    // $containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
 };
