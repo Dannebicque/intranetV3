4db0c6b31cf6f03d301118828285e3b0bb292321
davidannebicque <david.annebicque@gmail.com>
2022-05-06 18:19:25 +0200

    fix: [Widget] Remove deprecated : type de sortie

--- src/Components/Widget/DTO/WidgetView.php
+++ src/Components/Widget/DTO/WidgetView.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
- * @file /Users/davidannebicque/htdocs/intranetV3/src/Components/Widget/DTO/WidgetView.php
+ * Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/intranetV3/src/Components/Widget/DTO/WidgetView.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 08/10/2021 19:11
+ * @lastUpdate 06/05/2022 18:18
  */
 
 namespace App\Components\Widget\DTO;
@@ -12,6 +12,7 @@ namespace App\Components\Widget\DTO;
 use ArrayIterator;
 use Countable;
 use IteratorAggregate;
+use Traversable;
 
 class WidgetView implements IteratorAggregate, Countable
 {
@@ -35,7 +36,7 @@ class WidgetView implements IteratorAggregate, Countable
         }
     }
 
-    public function getIterator(): iterable
+    public function getIterator(): Traversable
     {
         return new ArrayIterator($this->children);
     }
