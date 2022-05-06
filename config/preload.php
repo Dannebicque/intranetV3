da05b033cdb11f8a726bc0d8a2a5ee6d88aa28d3
davidannebicque <david.annebicque@gmail.com>
2022-05-06 18:40:50 +0200

    fix: [Composer] fix recipe Symfony : framework.yaml

--- config/preload.php
+++ config/preload.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
- * @file /Users/davidannebicque/htdocs/intranetV3/config/preload.php
+ * Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/intranetV3/config/preload.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 25/07/2021 11:46
+ * @lastUpdate 06/05/2022 18:39
  */
 
-if (file_exists(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
+if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 /*
- * Copyright (c) 2021. | David Annebicque | IUT de Troyes  - All Rights Reserved
- * @file /Users/davidannebicque/htdocs/intranetV3/public/index.php
+ * Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/intranetV3/public/index.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 04/08/2021 08:02
+ * @lastUpdate 06/05/2022 18:40
  */
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function(array $context) {
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
