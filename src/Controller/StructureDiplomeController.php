b87a42b8e93f7577a865d6e306e86954c260877a
davidannebicque <david.annebicque@gmail.com>
2022-05-26 16:52:25 +0200

    feat: [APC] erreur sur les liens

--- src/Controller/StructureDiplomeController.php
+++ src/Controller/StructureDiplomeController.php
@@ -4,13 +4,14 @@
  * @file /Users/davidannebicque/Sites/intranetV3/src/Controller/StructureDiplomeController.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 07/05/2022 22:06
+ * @lastUpdate 26/05/2022 14:47
  */
 
 namespace App\Controller;
 
 use App\Exception\DiplomeNotFoundException;
 use App\Repository\DiplomeRepository;
+use App\Repository\PpnRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,19 +21,26 @@ class StructureDiplomeController extends AbstractController
 {
     #[Route('/structure/diplome/affiche', name: 'structure_diplome_affiche')]
     public function index(
+        PpnRepository $ppnRepository,
         DiplomeRepository $diplomeRepository,
         Request $request
     ): Response {
         $idDiplome = $request->query->get('diplome');
         $idPpn = $request->query->get('ppn');
         $diplome = $diplomeRepository->find($idDiplome);
+        $ppn = $ppnRepository->find($idPpn);
 
         if (null === $diplome) {
             throw new DiplomeNotFoundException();
         }
 
+        if (null === $ppn) {
+            $ppn = $diplome->getPpns()->first();
+        }
+
         return $this->render('structure_diplome/affiche.html.twig', [
             'diplome' => $diplome,
+            'ppn' => $ppn,
         ]);
     }
 }
