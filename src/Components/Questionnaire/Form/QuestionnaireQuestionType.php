89a3e864d2dc6020845e3d24b4a25d361eb1ae02
davidannebicque <david.annebicque@gmail.com>
2023-08-02 15:59:16 +0200

    feat: Change input type for Questionnaire Fields
    Changed the input field type from 'TextType' to 'TextareaType' for the 'libelle' attribute in QuestionnaireQuestionType and QuestionnaireSsQuestionType. This change was made to provide users with a larger, more flexible input area for their responses, improving overall usability. Added restrictions for textarea input, limiting it to 255 characters.

--- src/Components/Questionnaire/Form/QuestionnaireQuestionType.php
+++ src/Components/Questionnaire/Form/QuestionnaireQuestionType.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * Copyright (c) 2023. | David Annebicque | IUT de Troyes  - All Rights Reserved
  * @file /Users/davidannebicque/Sites/intranetV3/src/Components/Questionnaire/Form/QuestionnaireQuestionType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 11/09/2022 08:17
+ * @lastUpdate 02/08/2023 13:59
  */
 
 namespace App\Components\Questionnaire\Form;
@@ -13,6 +13,7 @@ use App\Entity\QuestQuestion;
 use App\Form\Type\YesNoType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,9 +29,10 @@ abstract class QuestionnaireQuestionType extends AbstractType
                 'choices' => $options['listeTypeQuestion'],
                 'attr' => ['data-action' => 'questionnaire--question#changeTypeQuestion']
             ])
-            ->add('libelle', TextType::class, [
+            ->add('libelle', TextareaType::class, [
                 'translation_domain' => 'form',
                 'label' => 'label.libelle',
+                'attr' => ['rows' => 3, 'maxlength' => 255],
             ])
             ->add('help', TextType::class, [
                 'label' => 'label.question.help',
