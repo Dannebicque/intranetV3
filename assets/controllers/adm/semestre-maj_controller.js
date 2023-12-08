62caf33e01cd33a45c3f8aa6efd46048b9212d86
davidannebicque <david.annebicque@gmail.com>
2023-12-08 14:11:14 +0100

    feac: nettoyage de code

--- assets/controllers/adm/semestre-maj_controller.js
+++ assets/controllers/adm/semestre-maj_controller.js
@@ -1,23 +1,22 @@
-import {Controller} from '@hotwired/stimulus'
+// Copyright (c) 2023. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// @file /Users/davidannebicque/Sites/intranetV3/assets/controllers/adm/semestre-maj_controller.js
+// @author davidannebicque
+// @project intranetV3
+// @lastUpdate 08/12/2023 14:11
+
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    static targets = ['bloc_etudiant']
+  static targets = ['bloc_etudiant']
 
-    static values = {
-        url: String,
-    }
+  static values = {
+    url: String,
+  }
 
-    connect() {
+  async toggle() {
+    this.bloc_etudiantTarget.innerHTML = ''
 
-    }
-
-    async toggle(event) {
-        console.log(event.detail.semestre)
-
-        this.bloc_etudiantTarget.innerHTML = '';
-
-        const response = await fetch(`${this.urlValue}`)
-        this.bloc_etudiantTarget.innerHTML = await response.text();
-    }
-
+    const response = await fetch(`${this.urlValue}`)
+    this.bloc_etudiantTarget.innerHTML = await response.text()
+  }
 }
--- assets/controllers/adm/semestre_controller.js
+++ assets/controllers/adm/semestre_controller.js
@@ -1,57 +1,58 @@
-import {Controller} from '@hotwired/stimulus'
+// Copyright (c) 2023. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// @file /Users/davidannebicque/Sites/intranetV3/assets/controllers/adm/semestre_controller.js
+// @author davidannebicque
+// @project intranetV3
+// @lastUpdate 08/12/2023 14:11
+
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    static targets = ['semestreModalZone']
+  static targets = ['semestreModalZone']
 
-    static values = {
-        url: String,
-        urlSave: String,
+  static values = {
+    url: String,
+    urlSave: String,
+  }
+
+  async toggle(event) {
+    const _value = event.currentTarget.value
+    const { actif } = event.currentTarget.dataset
+
+    const params = new URLSearchParams({
+      // semestre: _value,
+      actif,
+    })
+    const response = await fetch(`${this.urlValue}?${params.toString()}`)
+    // Met à jour la vue avec les données du semestre mis à jour.
+    this.updateView(_value)
+
+    this.dispatch(`semestre:updated:${_value}`, {
+      detail: {
+        semestre: _value,
+      },
+    })
+  }
+
+  updateView(semestre) {
+    // Sélectionne les éléments nécessaires dans le DOM.
+    const semestreActifElement = this.element.querySelector('.semestre-active')
+    const semestreActifBtn = this.element.querySelector('.semestre-btn-active')
+
+    // Met à jour le contenu de l'élément avec les nouvelles valeurs.
+    if (semestreActifElement.textContent === 'Actif') {
+      semestreActifElement.textContent = 'Inactif'
+      semestreActifElement.classList.remove('bg-success')
+      semestreActifElement.classList.add('bg-danger')
+      semestreActifBtn.classList.remove('fa-eye-slash')
+      semestreActifBtn.classList.add('fa-eye')
+      semestreActifBtn.dataset.actif = 0
+    } else {
+      semestreActifElement.textContent = 'Actif'
+      semestreActifElement.classList.remove('bg-danger')
+      semestreActifElement.classList.add('bg-success')
+      semestreActifBtn.classList.remove('fa-eye')
+      semestreActifBtn.classList.add('fa-eye-slash')
+      semestreActifBtn.dataset.actif = 1
     }
-
-    connect() {
-        // console.log('Hello from semestre controller')
-    }
-
-    async toggle(event) {
-        const _value = event.currentTarget.value
-        const actif = event.currentTarget.dataset.actif
-
-        const params = new URLSearchParams({
-            // semestre: _value,
-            actif: actif,
-        })
-        const response = await fetch(`${this.urlValue}?${params.toString()}`)
-        // Met à jour la vue avec les données du semestre mis à jour.
-        this.updateView(_value)
-
-        this.dispatch('semestre:updated:'+_value, {
-            detail: {
-                semestre: _value,
-            },
-        })
-    }
-
-    updateView(semestre) {
-        // Sélectionne les éléments nécessaires dans le DOM.
-        const semestreActifElement = this.element.querySelector('.semestre-active');
-        const semestreActifBtn = this.element.querySelector('.semestre-btn-active');
-
-        // Met à jour le contenu de l'élément avec les nouvelles valeurs.
-        if (semestreActifElement.textContent === 'Actif') {
-            semestreActifElement.textContent = 'Inactif';
-            semestreActifElement.classList.remove('bg-success');
-            semestreActifElement.classList.add('bg-danger');
-            semestreActifBtn.classList.remove('fa-eye-slash');
-            semestreActifBtn.classList.add('fa-eye');
-            semestreActifBtn.dataset.actif = 0;
-
-        } else {
-            semestreActifElement.textContent = 'Actif';
-            semestreActifElement.classList.remove('bg-danger');
-            semestreActifElement.classList.add('bg-success');
-            semestreActifBtn.classList.remove('fa-eye');
-            semestreActifBtn.classList.add('fa-eye-slash');
-            semestreActifBtn.dataset.actif = 1;
-        }
-    }
+  }
 }
