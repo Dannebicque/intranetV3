df21378418f9e16c3245243d840282c90dd8e813
davidannebicque <david.annebicque@gmail.com>
2024-02-16 22:23:50 +0100

    fix: Absence Justificatif, correction formulaire + liste des semestres

--- src/Controller/administration/AbsenceJustificatifController.php
+++ src/Controller/administration/AbsenceJustificatifController.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Copyright (c) 2023. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
  * @file /Users/davidannebicque/Sites/intranetV3/src/Controller/administration/AbsenceJustificatifController.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 13/12/2023 16:51
+ * @lastUpdate 16/02/2024 22:17
  */
 
 namespace App\Controller\administration;
@@ -22,7 +22,7 @@ use JsonException;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[Route('/administration/absence/justificatif')]
