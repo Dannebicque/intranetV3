493d4997866657619367cae4cff2ee7efa412545
davidannebicque <david.annebicque@gmail.com>
2024-04-09 11:41:21 +0200

    feat: recherche avec ctrl ou cmd + k

--- assets/controllers/global_controller.js
+++ assets/controllers/global_controller.js
@@ -2,11 +2,34 @@
 // @file /Users/davidannebicque/Sites/intranetV3/assets/controllers/global_controller.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 29/02/2024 20:40
+// @lastUpdate 09/04/2024 11:41
 
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['search']
+
   connect() {
+    document.addEventListener('keydown', this.handleKeydown.bind(this))
+    const os = (navigator.platform.match(/mac|win|linux/i) || ['unknown'])[0].toLowerCase()
+    if (os === 'mac') {
+      this.searchTarget.innerText = 'cmd+k'
+    } else {
+      this.searchTarget.innerText = 'ctrl+k'
+    }
+  }
+
+  disconnect() {
+    document.removeEventListener('keydown', this.handleKeydown.bind(this))
+  }
+
+  handleKeydown(event) {
+    // meta for Mac, ctrl for Linux/Windows
+    const pressedCtrl = event.metaKey || event.ctrlKey
+    const pressedKey = event.key
+    if (pressedCtrl && pressedKey === 'k') {
+      // dispatch event
+      this.dispatch('openSearch')
+    }
   }
 }
