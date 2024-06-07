88435c76765f3dbfb866e78c00adfd7d3654b6ac
davidannebicque <david.annebicque@gmail.com>
2024-06-07 08:43:39 +0200

    refac: Nettoyage planning rattrapage non utilisé

--- assets/js/pages/adm.justificatifs.js
+++ assets/js/pages/adm.justificatifs.js
@@ -1,31 +1,39 @@
-// Copyright (c) 2023. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/Sites/intranetV3/assets/js/pages/adm.justificatifs.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 13/12/2023 16:48
+// @lastUpdate 07/06/2024 08:43
 import $ from 'jquery'
 import Routing from 'fos-router'
 import { addCallout } from '../util'
 
-$(document).on('click', '.justificatif-accepte', function (e) {
-  const justificatif = $(this).data('justificatif')
-  $.ajax({
-    url: Routing.generate('administration_absence_justificatif_change_etat', { uuid: justificatif, etat: 'A' }),
-    success(e) {
-      const bx = $(`.bx_${justificatif}`)
-      const parent = bx.parent()
-      bx.remove()
-      let html = `<a href="#" class="btn btn-success btn-outline bx_${justificatif}">Demande Acceptée</a>`
-      html = `${html}<button data-justificatif="${justificatif}"
-        class="btn btn-warning btn-outline btn-square justificatif-annuler bx_${justificatif}" data-provide="tooltip" data-placement="bottom"
-        title="Annuler">
-          <i class="fas fa-rotate-left"></i></button>`
-      parent.prepend(html)
-      addCallout('Justificatif d\'absence validé !', 'success')
-    },
-    error(e) {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
+document.querySelectorAll('.justificatif-accepte').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+    const justificatif = e.getAttribute('data-justificatif')
+    fetch(Routing.generate('administration_absence_justificatif_change_etat', { uuid: justificatif, etat: 'A' }), {
+      method: 'POST',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          addCallout('Une erreur est survenue !', 'danger')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        const bx = document.querySelector(`.bx_${justificatif}`)
+        const parent = bx.parentElement
+        bx.remove()
+
+        let html = `<a href="#" class="btn btn-success btn-outline bx_${justificatif}">Demande Acceptée</a>`
+        html += `<button data-justificatif="${justificatif}" class="btn btn-warning btn-outline btn-square justificatif-annuler bx_${justificatif}" data-provide="tooltip" data-placement="bottom" title="Annuler"><i class="fas fa-rotate-left"></i></button>`
+
+        parent.insertAdjacentHTML('afterbegin', html)
+        addCallout('Justificatif d\'absence validé !', 'success')
+      })
+      .catch((error) => {
+        addCallout('Une erreur est survenue !', 'danger')
+      })
   })
 })
 
--- assets/js/pages/adm.rattrapages.js
+++ assets/js/pages/adm.rattrapages.js
@@ -1,131 +1,65 @@
-// Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+// Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
 // @file /Users/davidannebicque/Sites/intranetV3/assets/js/pages/adm.rattrapages.js
 // @author davidannebicque
 // @project intranetV3
-// @lastUpdate 07/07/2022 13:30
-import $ from 'jquery'
-import { addCallout } from '../util'
+// @lastUpdate 07/06/2024 08:43
 import Routing from 'fos-router'
+import { addCallout } from '../util'
 
-$(document).on('click', '.rattrapage-accepte', function (e) {
-  e.preventDefault()
-  const rattrapage = $(this).data('rattrapage')
-  $.ajax({
-    url: Routing.generate('administration_rattrapage_change_etat', { uuid: rattrapage, etat: 'a' }),
-    success() {
-      const bx = $(`.bx_${rattrapage}`)
-      const parent = bx.parent()
-      bx.remove()
-      parent.prepend('<a href="#" class="btn btn-success btn-outline"><i class="ti-check"></i>Acceptée</a>')
-      addCallout('Demande de rattrapage validée !', 'success')
-    },
-    error() {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
-  })
-})
+document.querySelectorAll('.rattrapage-accepte').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+    const rattrapage = e.getAttribute('data-rattrapage')
 
-$(document).on('click', '.rattrapage-refuse', function (e) {
-  e.preventDefault()
-  const rattrapage = $(this).data('rattrapage')
-  $.ajax({
-    url: Routing.generate('administration_rattrapage_change_etat', { uuid: rattrapage, etat: 'r' }),
-    success() {
-      const bx = $(`.bx_${rattrapage}`)
-      const parent = bx.parent()
-      bx.remove()
-      parent.prepend('<a href="#" class="btn btn-warning btn-outline"><i class="ti-na"></i>Refusée</a>')
-      addCallout('Demande de rattrapage refusée !', 'success')
-    },
-    error() {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
-  })
-})
-
-$(document).on('change', '.dateChange', function () {
-  const rattrapage = $(this).data('rattrapage')
-  $.ajax({
-    url: Routing.generate('administration_rattrapage_planning_change', { uuid: rattrapage, type: 'date' }),
-    data: {
-      data: $(this).val(),
-    },
-    method: 'POST',
-    error() {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
-  })
-})
-
-$(document).on('blur', '.salleChange', function () {
-  const rattrapage = $(this).data('rattrapage')
-  $.ajax({
-    url: Routing.generate('administration_rattrapage_planning_change', { uuid: rattrapage, type: 'salle', data: $(this).val() }),
-    error(e) {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
-  })
-})
-
-$(document).on('blur', '.heureChange', function () {
-  const rattrapage = $(this).data('rattrapage')
-  $.ajax({
-    url: Routing.generate('administration_rattrapage_planning_change', { uuid: rattrapage, type: 'heure', data: $(this).val() }),
-    error() {
-      addCallout('Une erreur est survenue !', 'danger')
-    },
-  })
-})
-
-$(document).on('click', '#sallePartout', () => {
-  const salle = $('#salle').val()
-  $.ajax({
-    // sauvegarde de la salle pour les rattrapages du diplôme
-    url: Routing.generate('administration_rattrapage_update_global', { type: 'salle', diplome }),
-    data: {
-      valeur: salle,
-    },
-    method: 'POST',
-    success() {
-      $('.salleChange').each(function () {
-        $(this).val(salle)
+    fetch(Routing.generate('administration_rattrapage_change_etat', { uuid: rattrapage, etat: 'a' }))
+      .then((response) => {
+        // handle success
+        if (response.ok) {
+          const bx = document.querySelector(`.bx_${rattrapage}`)
+          const parent = bx.parentElement
+          bx.parentNode.removeChild(bx)
+          const a = document.createElement('a')
+          a.setAttribute('href', '#')
+          a.classList.add('btn', 'btn-success', 'btn-outline')
+          parent.insertBefore(a, parent.firstChild)
+          addCallout('Demande de rattrapage validée !', 'success')
+        } else {
+          addCallout('Une erreur est survenue !', 'danger')
+        }
       })
-    },
-  })
-})
-
-$(document).on('click', '#datePartout', (e) => {
-  e.preventDefault()
-  const date = $('#date').val()
-  $.ajax({
-    // sauvegarde de la salle pour les rattrapages du diplôme
-    url: Routing.generate('administration_rattrapage_update_global', { type: 'date', diplome }),
-    data: {
-      valeur: date,
-    },
-    method: 'POST',
-    success() {
-      $('.dateChange').each(function () {
-        $(this).val(date)
+      .catch((error) => {
+        // handle error
+        addCallout('Une erreur est survenue !', 'danger')
       })
-    },
-  })
-})
+  });
+});
 
-$(document).on('click', '#heurePartout', (e) => {
-  e.preventDefault()
-  const heure = $('#heure').val()
-  $.ajax({
-    // sauvegarde de la salle pour les rattrapages du diplôme
-    url: Routing.generate('administration_rattrapage_update_global', { type: 'heure', diplome }),
-    data: {
-      valeur: heure,
-    },
-    method: 'POST',
-    success() {
-      $('.heureChange').each(function () {
-        $(this).val(heure)
+document.querySelectorAll('.rattrapage-refuse').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const rattrapage = e.getAttribute('data-rattrapage')
+    const url = Routing.generate('administration_rattrapage_change_etat', { uuid: rattrapage, etat: 'r' })
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`)
+        }
+
+        const bx = document.querySelector(`.bx_${rattrapage}`)
+        const parent = bx.parentNode
+        bx.remove()
+        const anchor = document.createElement('a')
+        anchor.href = '#'
+        anchor.className = 'btn btn-warning btn-outline'
+        anchor.innerHTML = '<i class="ti-na"></i>Refusée'
+
+        parent.prepend(anchor)
+        addCallout('Demande de rattrapage refusée !', 'success')
       })
-    },
+      .catch((error) => {
+        addCallout('Une erreur est survenue !', 'danger')
+      });
   })
-})
+});
