084e6fb58a7eaee5762bc149ccab1197106b53eb
davidannebicque <david.annebicque@gmail.com>
2024-06-08 16:19:08 +0200

    fix: temporaire pour partie projet

--- src/Form/stage/AdresseType.php
+++ src/Form/stage/AdresseType.php
@@ -1,22 +1,18 @@
 <?php
 /*
- * Copyright (c) 2022. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
  * @file /Users/davidannebicque/Sites/intranetV3/src/Form/stage/AdresseType.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 01/12/2022 21:21
+ * @lastUpdate 08/06/2024 16:19
  */
 
 namespace App\Form\stage;
 
 use App\Entity\Adresse;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\Exception\AccessException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -28,86 +24,88 @@ class AdresseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        //todo: remettre disable a true et gérer pour la partie projet...
         $builder
             ->add('adresse1', TextType::class, [
                 'label' => 'Adresse (numéro, rue) * :',
-                'disabled' => true,
+                'disabled' => false,
                 'attr' => ['autocomplete' => 'off'],
                 'constraints' => [new NotBlank(message: 'Veuillez renseigner ce champ')],
             ])
             ->add('adresse2', TextType::class, [
                 'label' => 'Suite adresse (étage, bâtiment, ...) :',
-                'disabled' => true,
+                'disabled' => false,
                 'required' => false,
                 'attr' => ['autocomplete' => 'off'],
             ])
             ->add('adresse3', TextType::class, [
                 'label' => 'Complément d’adresse :',
-                'disabled' => true,
+                'disabled' => false,
                 'required' => false,
                 'attr' => ['autocomplete' => 'off'],
             ])
             ->add('code_postal', TextType::class, [
                 'label' => 'Code Postal * :',
-                'disabled' => true,
+                'disabled' => false,
                 'constraints' => [new NotBlank(message: 'Veuillez renseigner ce champ')],
                 'attr' => ['autocomplete' => 'off'],
             ])
-            ->add('ville', ChoiceType::class, [
+            ->add('ville', TextType::class, [ //todo: remettre choice
                 'label' => 'Ville * :',
-                'disabled' => true,
-                'attr' => ['autocomplete' => 'off'],
-                'constraints' => [new NotBlank(message: 'Veuillez renseigner ce champ')],
+                'disabled' => false,
+                // 'attr' => ['autocomplete' => 'off'],
+                // 'constraints' => [new NotBlank(message: 'Veuillez renseigner ce champ')],
             ])
-            ->addEventListener(
-                FormEvents::PRE_SUBMIT,
-                [$this, 'onPreSubmit']
-            )
-            ->addEventListener(
-                FormEvents::PRE_SET_DATA,
-                [$this, 'onPreSetData']
-            )
-            ->add('modif_adresse', ButtonType::class, ['label' => 'Modifier l\'adresse', 'attr' => ['class' => 'btn']]);
+//            ->addEventListener(
+//                FormEvents::PRE_SUBMIT,
+//                [$this, 'onPreSubmit']
+//            )
+//            ->addEventListener(
+//                FormEvents::PRE_SET_DATA,
+//                [$this, 'onPreSetData']
+//            )
+//            ->add('modif_adresse', ButtonType::class, ['label' => 'Modifier l\'adresse', 'attr' => ['class' => 'btn']])
+        ;
     }
 
-    public function onPreSubmit(FormEvent $event): void
-    {
-        if (null === $event->getData()) {
-            return;
-        }
-        if (array_key_exists('adresse1', $event->getData())) {
-            $input = $event->getData()['adresse1'];
-            $event->getForm()->add('adresse1', TextType::class, ['empty_data' => $input]);
-        }
-        if (array_key_exists('adresse2', $event->getData())) {
-            $input = $event->getData()['adresse2'];
-            $event->getForm()->add('adresse2', TextType::class, ['empty_data' => $input]);
-        }
-        if (array_key_exists('adresse3', $event->getData())) {
-            $input = $event->getData()['adresse3'];
-            $event->getForm()->add('adresse3', TextType::class, ['empty_data' => $input]);
-        }
-        if (array_key_exists('code_postal', $event->getData())) {
-            $input = $event->getData()['code_postal'];
-            $event->getForm()->add('code_postal', TextType::class, ['empty_data' => $input]);
-        }
-        if (array_key_exists('ville', $event->getData())) {
-            $input = $event->getData()['ville'];
-            $event->getForm()->add('ville', ChoiceType::class, ['choices' => [$input]]);
-        }
-    }
-
-    public function onPreSetData(FormEvent $event): void
-    {
-        $ville = $event->getData();
-        $form = $event->getForm();
-        if (null !== $ville) {
-            $form->add('ville', ChoiceType::class, [
-                'choices' => [$ville->getVille() => $ville->getVille()],
-                'disabled' => true,
-            ]);
-        }
-    }
+//    public function onPreSubmit(FormEvent $event): void
+//    {
+//        if (null === $event->getData()) {
+//            return;
+//        }
+//        if (array_key_exists('adresse1', $event->getData())) {
+//            $input = $event->getData()['adresse1'];
+//            $event->getForm()->add('adresse1', TextType::class, ['empty_data' => $input]);
+//        }
+//        if (array_key_exists('adresse2', $event->getData())) {
+//            $input = $event->getData()['adresse2'];
+//            $event->getForm()->add('adresse2', TextType::class, ['empty_data' => $input]);
+//        }
+//        if (array_key_exists('adresse3', $event->getData())) {
+//            $input = $event->getData()['adresse3'];
+//            $event->getForm()->add('adresse3', TextType::class, ['empty_data' => $input]);
+//        }
+//        if (array_key_exists('code_postal', $event->getData())) {
+//            $input = $event->getData()['code_postal'];
+//            $event->getForm()->add('code_postal', TextType::class, ['empty_data' => $input]);
+//        }
+//        if (array_key_exists('ville', $event->getData())) {
+//            $input = $event->getData()['ville'];
+//            $event->getForm()->add('ville', ChoiceType::class, ['choices' => [$input]]);
+//        }
+//    }
+//
+//    public function onPreSetData(FormEvent $event): void
+//    {
+//        $ville = $event->getData();
+//        $form = $event->getForm();
+//        if (null !== $ville) {
+//            $form->add('ville', ChoiceType::class, [
+//                'choices' => [$ville->getVille() => $ville->getVille()],
+//                'disabled' => true,
+//            ]);
+//        }
+//    }
 
     /**
      * @throws AccessException
