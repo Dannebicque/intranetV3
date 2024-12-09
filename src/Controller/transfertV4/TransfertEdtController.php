9cefd89b049fefeca0c8635c770df53c538a65e6
davidannebicque <david.annebicque@gmail.com>
2024-12-09 08:24:58 +0100

    feat: Transfert vers V4

---
+++ src/Controller/transfertV4/TransfertEdtController.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/intranetV3/src/Controller/transfertV4/TransfertEdtController.php
+ * @author davidannebicque
+ * @project intranetV3
+ * @lastUpdate 09/12/2024 08:24
+ */
+
+namespace App\Controller\transfertV4;
+
+use App\Repository\EdtPlanningRepository;
+use App\Repository\EtudiantRepository;
+use App\Repository\ScolariteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/transfert')]
+class TransfertEdtController extends AbstractController
+{
+    /**
+     * @param EtudiantRepository $etudiantRepository
+     * @param ScolariteRepository $scolariteRepository
+     * @param int $id
+     * @return Response
+     */
+    #[Route('/edt-intranet', name: 'api_transfert_edt_intranet', methods: ['GET', 'POST'])]
+    public function getEdtIntranet(
+        EdtPlanningRepository $edtIntranetRepository,
+    ): Response
+    {
+        $edtIntranet = $edtIntranetRepository->findBy(['anneeUniversitaire' => 7]);
+        $tabEdtIntranet = [];
+        if ($edtIntranet) {
+            foreach ($edtIntranet as $edt) {
+                // fusionner la date avec l'heure de début
+                $debut = $edt->getDate()->format('Y-m-d') . ' ' . $edt->getHeureDebut()?->format('H:i:s');
+                $fin = $edt->getDate()->format('Y-m-d') . ' ' . $edt->getHeureFin()?->format('H:i:s');
+                $tabEdtIntranet[] = [
+                    'id' => $edt->getId(),
+                    'debut' => $debut,
+                    'fin' => $fin,
+                    'matiere' => $edt->getTypeIdMatiere(),
+                    'salle' => $edt->getSalle(),
+                    'prof' => $edt->getIntervenant()?->getId(),
+                    'libprof' => $edt->getIntervenant()?->getDisplayPr(),
+                    'groupe' => $edt->getGroupeObjet()?->getId(),
+                    'type' => $edt->getType(),
+                    'couleur' => $edt->getSemestre()?->getAnnee()?->getCouleur(),
+                    'commentaire' => $edt->getCommentaire(),
+                    'evaluation' => $edt->getEvaluation(),
+                    'codeRh' => $edt->getIntervenant()?->getNumeroHarpege(),
+                    'codeGroupe' => $edt->getGroupeObjet()?->getCodeApogee(),
+                    'jour' => $edt->getJour(),
+                    'libGroupe' => $edt->getGroupeObjet()?->getLibelle(),
+                    'semestre' => $edt->getSemestre()?->getId(),
+                ];
+            }
+        }
+
+        return $this->json($tabEdtIntranet);
+    }
+}
---
+++ src/Controller/transfertV4/TransfertEtudiantController.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Copyright (c) 2024. | David Annebicque | IUT de Troyes  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/intranetV3/src/Controller/transfertV4/TransfertEtudiantController.php
+ * @author davidannebicque
+ * @project intranetV3
+ * @lastUpdate 09/12/2024 08:24
+ */
+
+namespace App\Controller\transfertV4;
+
+use App\Repository\EtudiantRepository;
+use App\Repository\ScolariteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/transfert')]
+class TransfertEtudiantController extends AbstractController
+{
+    /**
+     * @param EtudiantRepository $etudiantRepository
+     * @param ScolariteRepository $scolariteRepository
+     * @param int $id
+     * @return Response
+     */
+    #[Route('/etudiant/{id}', name: 'api_transfert_etudiant', methods: ['GET', 'POST'])]
+    public function getScolaritesEtudiant(
+        EtudiantRepository  $etudiantRepository,
+        ScolariteRepository $scolariteRepository,
+        int                 $id
+    ): Response
+    {
+        $etudiant = $etudiantRepository->find($id);
+        $tabScolarites = [];
+        if ($etudiant) {
+            // si une scolarité existe sur l'année universitaire en cours on prend, sinon on en ajoute une
+            // on récupere les autres scolarités
+            $scolarites = $scolariteRepository->findBy(['etudiant' => $etudiant]);
+            $anneeUniversitaire = $etudiant->getAnneeUniversitaire();
+            $scolEnCours = false;
+            $ordre = 0;
+            foreach ($scolarites as $scolarite) {
+                if ($scolarite->getAnneeUniversitaire() === $anneeUniversitaire) {
+                    $scolEnCours = true;
+                }
+                $tabScolarites[] = [
+                    'id' => $scolarite->getId(),
+                    'annee' => $scolarite->getAnneeUniversitaire()?->getId(),
+                    'semestre' => $scolarite->getSemestre()?->getId(),
+                    'ordre' => $scolarite->getOrdre(),
+                    'decision' => $scolarite->getDecision(),
+                    'proposition' => $scolarite->getProposition(),
+                    'moyenne' => $scolarite->getMoyenne(),
+                    'nbAbsences' => $scolarite->getNbAbsences(),
+                    'commentaire' => $scolarite->getCommentaire(),
+                    'diffuse' => $scolarite->getDiffuse(),
+                    'moyennesMatieres' => $scolarite->getMoyennesMatieres(),
+                    'moyennesUes' => $scolarite->getMoyennesUes()
+                ];
+                $ordre = max($ordre, $scolarite->getOrdre());
+            }
+
+            if ($scolEnCours === false) {
+                //on ajoute la scolarité pour l'année courante
+                $tabScolarites[] = [
+                    'id' => null,
+                    'annee' => $anneeUniversitaire?->getId(),
+                    'semestre' => $etudiant->getSemestre()?->getId(),
+                    'ordre' => $ordre + 1,
+                    'decision' => null,
+                    'proposition' => null,
+                    'moyenne' => null,
+                    'nbAbsences' => 0,
+                    'commentaire' => null,
+                    'diffuse' => false,
+                    'moyennesMatieres' => [],
+                    'moyennesUes' => []
+                ];
+            }
+
+            return $this->json($tabScolarites);
+        }
+
+
+        return $this->json([]);
+    }
+}
