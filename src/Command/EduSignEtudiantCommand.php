e98758d16f82d374853aabcdfb656d5cff8330c8
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-20 09:03:40 +0100

    fix: commande change semestre

--- src/Command/EduSignEtudiantCommand.php
+++ src/Command/EduSignEtudiantCommand.php
@@ -11,8 +11,10 @@
 namespace App\Command;
 
 use App\Classes\EduSign\UpdateEtudiant;
+use App\Repository\DiplomeRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -25,6 +27,7 @@ class EduSignEtudiantCommand extends Command
 {
     public function __construct(
         private readonly UpdateEtudiant $updateEtudiant,
+        private readonly DiplomeRepository $diplomeRepository
     )
     {
         parent::__construct();
@@ -33,14 +36,18 @@ class EduSignEtudiantCommand extends Command
 
     protected function configure(): void
     {
-
+        $this
+            ->addArgument('keyEduSign', InputArgument::OPTIONAL, 'Clé EduSign');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $this->updateEtudiant->update();//boucler sur département pour chaque update (ou diplome)
+        $keyEdusign = $input->getArgument('keyEduSign');
+        $diplome = $this->diplomeRepository->findOneBy(['keyEduSign' => $keyEdusign]);
+
+        $this->updateEtudiant->changeSemestre($diplome, $keyEdusign);//boucler sur département pour chaque update (ou diplome)
 
         $io->success('Etudiants mis à jour sur EduSign.');
 
