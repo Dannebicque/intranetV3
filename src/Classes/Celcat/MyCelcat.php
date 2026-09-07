6e1eeb4f8e629791304ee7bdbe66ea600c1df942
davidannebicque <david.annebicque@gmail.com>
2026-09-07 10:28:21 +0200

    feat: Celcat récupération des événements hors cours

--- src/Classes/Celcat/MyCelcat.php
+++ src/Classes/Celcat/MyCelcat.php
@@ -4,7 +4,7 @@
  * @file /Users/davidannebicque/Sites/intranetV3/src/Classes/Celcat/MyCelcat.php
  * @author davidannebicque
  * @project intranetV3
- * @lastUpdate 07/09/2026 10:26
+ * @lastUpdate 07/09/2026 10:28
  */
 
 namespace App\Classes\Celcat;
@@ -401,7 +401,7 @@ class MyCelcat
                     // non cours
                     $notes = odbc_result($resultCelcat, 17);
                     $event->setCodeModule($eventId);
-                    $event->setLibModule(utf8_encode(odbc_result($resultCelcat, 6)) + ' ' + utf8_encode($notes));
+                    $event->setLibModule(utf8_encode(odbc_result($resultCelcat, 6)) . ' ' . utf8_encode($notes));
                     $event->setType($this->tGroupes[$codeGroupe]?->getTypeGroupe()->getType() ?? 'CM');
                 } else {
                     $event->setCodeModule($codeModule);
